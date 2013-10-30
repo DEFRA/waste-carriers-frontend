08181efc35cb3ed7d618e16b3d631771b9a060ea
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-10-30 23:11:56 +0000

    Sendgrid configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Registrations::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV[SENDGRID_USERNAME],
-    :password => ENV[SENDGRID_PASSWORD],
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
     :domain => 'www.dev.wastecarriers.service.gov.uk',
     :address => 'smtp.sendgrid.net',
     :port => 587,
