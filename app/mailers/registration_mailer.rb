2526a01265df71a34c1096edf46449a6d1493764
steven-radford <stevenr@aptosolutions.co.uk>
2013-11-01 14:48:59 +0000

    Update emails to include registration information

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,9 +1,10 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "waste-carrier-registrations@example.com"
 
-  def welcome_email(user)
+  def welcome_email(user, registration)
   	@user = user
   	@url = 'http://37.26.89.99/registrations'
+  	@registration = registration
     mail(to: @user.email, subject: 'Registration Complete for a Waste Carrier!')
   end
   
