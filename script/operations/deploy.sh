defd6fe6631bbe8eddade0193edc296a59479b03
Rob Hoffman <hoffmanrob@yahoo.com>
2013-11-01 17:46:02 +0000

    Correcting a slew of outdated variable names.

--- script/operations/deploy.sh
+++ script/operations/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash --login
 #set -x
 
 ## This script will deploy and start the we-services application.
@@ -31,14 +31,14 @@ if [[ -z "${WCRS_FRONTEND_PUBLIC_APP_DOMAIN}" ]]; then env_alert WCRS_FRONTEND_P
 #if [[ -z "${WCRS_FRONTEND_ADMIN_APP_DOMAIN}" ]]; then env_alert WCRS_FRONTEND_ADMIN_APP_DOMAIN; fi
 
 ## Stop nginx.
+echo "Stopping nginx."
 sudo service nginx stop
 
 ## Backup the current database.
-if [ -d "${WESERVICES_HOME}/live/db" ]; then
+if [ -f "${WCRS_FRONTEND_HOME}/live/db/${WCRS_FRONTEND_SQLITE_FILE}" ]; then
   echo "Backing up current database."
-  cd "${WESERVICES_HOME}/live/db" 
-  tar zcf "${WCRS_FRONTEND_SQLITE_FILE}-${DATESTAMP}.tgz" \
-          "${WCRS_FRONTEND_SQLITE_FILE}"
+  cd "${WCRS_FRONTEND_HOME}/live/db" 
+  tar zcf "${WCRS_FRONTEND_SQLITE_FILE}-${DATESTAMP}.tgz" "${WCRS_FRONTEND_SQLITE_FILE}"
   if [ $? -ne 0 ]; then
     echo "ERROR: Backup of ${WCRS_FRONTEND_SQLITE_FILE} failed."
     echo "       Exiting now. Nginx is stopped. No new code has been deployed."
@@ -51,39 +51,44 @@ RELEASE_DIR="wcrs-frontend-${DATESTAMP}"
 echo "Creating new release directory ${RELEASE_DIR}"
 mkdir "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}"
 cd "${WCRS_FRONTEND_HOME}"
-if [ -d "${WESERVICES_HOME}/live" ]; then
+if [ -d "${WCRS_FRONTEND_HOME}/live" ]; then
   rm live
 fi
 ln -s "${RELEASE_DIR}" live
 
 ## Deploy the new code.
 echo "Copying new code to ${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/"
-cp -pr "${WCRS_FRONTEND_SOURCE}/*" "${WESERVICES_HOME}/${RELEASE_DIR}/"
+cp -pr ${WCRS_FRONTEND_SOURCE}/* "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/"
 
 ## Replace environment variables.
+echo "Setting environment variables in files."
 sed -i "s/WCRS_FRONTEND_WCRS_SERVICES_URL/${WCRS_FRONTEND_WCRS_SERVICES_URL}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/application.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/application.rb"
 sed -i "s/WCRS_FRONTEND_PUBLIC_APP_DOMAIN/${WCRS_FRONTEND_PUBLIC_APP_DOMAIN}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/environments/development.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/environments/development.rb"
 sed -i "s/WCRS_FRONTEND_EMAIL_HOST/${WCRS_FRONTEND_EMAIL_HOST}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/environments/development.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/environments/development.rb"
 sed -i "s/WCRS_FRONTEND_EMAIL_PORT/${WCRS_FRONTEND_EMAIL_PORT}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/environments/development.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/environments/development.rb"
 sed -i "s/WCRS_FRONTEND_EMAIL_USERNAME/${WCRS_FRONTEND_EMAIL_USERNAME}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/environments/development.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/environments/development.rb"
 sed -i "s/WCRS_FRONTEND_EMAIL_PASSWORD/${WCRS_FRONTEND_EMAIL_PASSWORD}/g" \
-       "${WESERVICES_HOME}/${RELEASE_DIR}/conf/environments/development.rb"
+       "${WCRS_FRONTEND_HOME}/${RELEASE_DIR}/config/environments/development.rb"
 
 ## Bundle and rake.
+echo "Installing bundle."
 cd "${WCRS_FRONTEND_HOME}/live"
-rvm use ${WCRS_FRONTEND_RUBY_VERSION}
+rvm use "${WCRS_FRONTEND_RUBY_VERSION}"
 bundle install
+echo "Migrating database changes, if any."
 rake db:migrate RAILS_ENV="${WCRS_FRONTEND_RAILS_ENV}"
 
 ## Start nginx.
+echo "Starting nginx."
 sudo service nginx start
 
 ## Test.
+echo "Running tests."
 rake db:test:prepare
 xvfb-run cucumber -f json -o ${WCRS_FRONTEND_HOME}/live/features/reports/cucumber.json
 
