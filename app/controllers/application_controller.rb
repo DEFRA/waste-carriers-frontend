4ea0b67d3aff130b2877660b42fe64ee4263d337
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-11-05 17:59:39 +0000

    Redirect after logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
     #request.referrer
-    registrations_path
+    #registrations_path
+    root_path
   end
 
   def after_sign_in_path_for(resource)
