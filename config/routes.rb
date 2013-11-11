389e2feda19777fc55eb87f154cfe50b8f6a3379
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-11-11 18:09:29 +0000

    Reverting URL for user administration page, and not redirecting on page on not found

--- config/routes.rb
+++ config/routes.rb
@@ -35,16 +35,17 @@ Registrations::Application.routes.draw do
 
   resources :registrations
 
-  scope "/administration" do 
-    resources :agency_users
-  end
+  #scope "/administration" do 
+  #  resources :agency_users
+  #end
   
   resources :discovers
 
+  resources :agency_users
   # Add a route for a 404, Define this catch all unknowns last
-  if Rails.env.development?
-  	get "*path" => "registrations#notfound", via: [:get], :message => 'Page Not Found'
-  end
+  #if Rails.env.development?
+  #	get "*path" => "registrations#notfound", via: [:get], :message => 'Page Not Found'
+  #end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
