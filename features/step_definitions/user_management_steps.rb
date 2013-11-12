abe6a37bf2ef5d7e5ad7306b92bf776203ded69d
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-11-12 01:15:30 +0000

    Work on Cucumber tests for user administration

---
+++ features/step_definitions/user_management_steps.rb
@@ -0,0 +1,71 @@
+#step definitions for user administration
+
+Given(/^I am an administrator$/) do
+  #pending # express the regexp above with the code you wish you had
+
+  #TODO Create admin user if needed
+end
+
+When(/^I log in as administrator$/) do
+  visit "/admins/sign_in"
+  assert page.has_content? "Administration Login"
+  fill_in "admin_email", :with => 'admin@waste-exemplar.gov.uk'
+  fill_in "admin_password", :with => 'secret123'
+  click_button "Sign in"
+end
+
+Then(/^I should see the user administration page$/) do
+  assert page.has_content? "Signed in as admin"
+  assert page.has_content? "Listing agency users"
+end
+
+Given(/^I am logged in as an administrator$/) do
+  visit "/admins/sign_in"
+  assert page.has_content? "Administration Login"
+  fill_in "admin_email", :with => 'admin@waste-exemplar.gov.uk'
+  fill_in "admin_password", :with => 'secret123'
+  click_button "Sign in"
+end
+
+When(/^I elect to create a new agency user$/) do
+  click_button "New agency user"
+end
+
+When(/^I fill in valid agency user details$/) do
+  fill_in "agency_user_email", :with => 'joe@waste-exemplar.gov.uk'
+  fill_in "agency_user_password", :with => 'secret123'
+  click_button "Create Agency User"
+end
+
+Then(/^the user should have been created$/) do
+  assert(AgencyUser.where(email: 'joe@waste-exemplar.gov.uk').count > 0)
+end
+
+Then(/^I should see the user's details page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^there is a user to be deleted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I elect to delete the user$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the user should have been deleted$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am not logged in as an administrator$/) do
+  #Nothing to do
+end
+
+When(/^I access the user administration page$/) do
+  visit "/agency_users"
+end
+
+Then(/^I should be prompted to login as an administrator$/) do
+  assert page.has_content?("Administration Login")
+  assert !page.has_content?("Listing agency users")
+end
---
+++ features/user_management.feature
@@ -0,0 +1,36 @@
+Feature: User Management
+
+As an Administrator, I want to manage the accounts of internal agency users (including NCCC workers)
+so that these users can access the service and create and manage registrations on behalf of businesses.
+
+Administrators can create, modify and delete users.
+
+
+Scenario: Login as Administrator
+  Given I am an administrator
+  When I log in as administrator
+  Then I should see the user administration page
+
+
+Scenario: Create a new user
+  Given I am logged in as an administrator
+  When I elect to create a new agency user
+  And I fill in valid agency user details
+  And I click "Create Agency User"
+  Then the user should have been created
+  And I should see the user's details page
+
+
+Scenario: Delete a user
+  Given I am logged in as an administrator
+  And there is a user to be deleted
+  When I elect to delete the user
+  Then the user should have been deleted
+
+
+Scenario: Attempt to access user administration without being logged in
+  Given I am an administrator
+  When I am not logged in as an administrator
+  And I access the user administration page
+  Then I should be prompted to login as an administrator
+
