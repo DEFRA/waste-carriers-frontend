962e8e18837e506bc3a4b6c5f08b455c2f9441a5
steven-radford <stevenr@aptosolutions.co.uk>
2013-11-29 10:39:02 +0000

    ADD: Updated smart answers based on latest legal changes

--- app/models/discover.rb
+++ app/models/discover.rb
@@ -1,7 +1,7 @@
 class Discover
   include ActiveModel::Model
   
-  attr_accessor :id, :businessType, :otherBusinesses, :constructionWaste, :wasteType_animal, :wasteType_mine, :wasteType_farm, :wasteType_other, :wasteType_none
+  attr_accessor :id, :businessType, :otherBusinesses, :onlyAMF, :constructionWaste, :wasteType_animal, :wasteType_mine, :wasteType_farm, :wasteType_other, :wasteType_none
   
   # businessType must be present
   validates_presence_of :businessType
@@ -9,11 +9,15 @@ class Discover
   # otherBusinesses must be present if businessType is present and has a value of "soleTrader" || "partnership" || "limitedCompany"
   validates_presence_of :otherBusinesses, :if => lambda { |o| !o.businessType.nil? && (o.businessType == "soleTrader" || o.businessType == "partnership" || o.businessType == "limitedCompany") }
   
+  # onlyAMF must be present if otherBusinesses is no
+  validates_presence_of :onlyAMF, :if => lambda { |o| !o.otherBusinesses.nil? && o.otherBusinesses == "yes" }
+  
   # construction waste must be present if otherBusinesses is no
   validates_presence_of :constructionWaste, :if => lambda { |o| !o.otherBusinesses.nil? && o.otherBusinesses == "no" }
   
   # wastetype must be present if construction waste is no, plus ensure that all other checkboxes are not selected
-  validate :validate_not_apply, :if => lambda { |o| !o.constructionWaste.nil? && o.constructionWaste == "no" }
+  # Remove waste type validation as field removed
+  #validate :validate_not_apply, :if => lambda { |o| !o.constructionWaste.nil? && o.constructionWaste == "no" }
   
   def validate_not_apply
     if wasteType_none == "1"
@@ -39,37 +43,52 @@ class Discover
   
   def isUpper?
     # Check if submission is for upper tier
-    # 1. If other businesses is yes
+    # 1. If onlyAMF is no
     # 2. If constructionWaste is yes
-    # 3. If more than one wasteType is selected
-    if otherBusinesses == "yes"
-      Rails.logger.info "Is Upper Tier found (otherbusinesses)"
+    if onlyAMF == "no"
+      Rails.logger.info "Is Upper Tier found (onlyAMF)"
       true
     elsif constructionWaste == "yes"
       Rails.logger.info "Is Upper Tier found (constructionWaste)"
       true
     else 
-      # Count number of selected checkboxes
-      count = 0
-      if wasteType_animal == "1"
-        count = count + 1
-      end
-      if wasteType_mine == "1"
-        count = count + 1
-      end
-      if wasteType_farm == "1"
-        count = count + 1
-      end
-      if wasteType_other == "1"
-        count = count + 1
-      end
-      if count > 1
-        Rails.logger.info "Is Upper Tier found (multi wasteType)"
-        true
-      else
-        false
-      end
+      false
     end
   end
+  
+#  def isUpper?
+#    # Check if submission is for upper tier
+#    # 1. If other businesses is yes
+#    # 2. If constructionWaste is yes
+#    # 3. If more than one wasteType is selected
+#    if otherBusinesses == "yes"
+#      Rails.logger.info "Is Upper Tier found (otherbusinesses)"
+#      true
+#    elsif constructionWaste == "yes"
+#      Rails.logger.info "Is Upper Tier found (constructionWaste)"
+#      true
+#    else 
+#      # Count number of selected checkboxes
+#      count = 0
+#      if wasteType_animal == "1"
+#        count = count + 1
+#      end
+#      if wasteType_mine == "1"
+#        count = count + 1
+#      end
+#      if wasteType_farm == "1"
+#        count = count + 1
+#      end
+#      if wasteType_other == "1"
+#        count = count + 1
+#      end
+#      if count > 1
+#        Rails.logger.info "Is Upper Tier found (multi wasteType)"
+#        true
+#      else
+#        false
+#      end
+#    end
+#  end
   
 end
