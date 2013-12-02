46ae28504b4c1d65ffea49a53ec7be46f72cffdd
steven-radford <stevenr@aptosolutions.co.uk>
2013-12-02 11:06:19 +0000

    ADD: New validation message for individual fields

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,12 @@
 module RegistrationsHelper
+
+def validation_for(model, attribute)
+  if model.errors[attribute].any?
+    # Note: Calling raw() forces the characters to be un-escaped and thus HTML elements can be defined here
+    raw("<span class=\"error-text\">#{model.errors.full_messages_for(attribute).first}</span>")
+  else
+    ""
+  end
+end
+
 end
