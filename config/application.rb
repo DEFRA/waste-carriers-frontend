094e05b8f3efaa8a3f5acd850365912b3bf599a2
steven-radford <stevenr@aptosolutions.co.uk>
2013-12-04 10:07:20 +0000

    Update version of MVP+ branch to match latest sprint

--- config/application.rb
+++ config/application.rb
@@ -88,7 +88,7 @@ module Registrations
     # Update this whenever the reported version number is supposed to have changed - particularly before any new releases. 
     # Note: This is the version of the frontend application. The version number of the services application may change separately.
     # Use semantic versioning (Major.Minor.Patch)
-    config.application_version = '0.5.1'
+    config.application_version = '0.6.0'
 
     # The e-mail address shown on the Finish page and used in e-mails sent by the application
     config.registrations_service_email = 'registrations@wastecarriersregistration.service.gov.uk'
