7b93ce7fd7ce11a07371dd715deadb0765bdc427
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-12-10 18:43:24 +0000

    Uploading NCCC admins

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,52 +6,38 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first) 
 
-# TODO FIXME - Remove for production!!!
 
-if Admin.count == 0
-  admin = Admin.new(:email => 'admin@waste-exemplar.gov.uk', :password => 'secret123')
+if !Admin.find_by_email('jamie.dempster@environment-agency.gov.uk')
+  admin = Admin.new(:email => 'jamie.dempster@environment-agency.gov.uk', :password => AgencyUser.random_password)
   admin.save!
 end
-
-# TODO - Remove these after pen test
-if !Admin.find_by_email('admin1@waste-exemplar.gov.uk')
-  admin = Admin.new(:email => 'admin1@waste-exemplar.gov.uk', :password => 'MyS3c3t!')
+if !Admin.find_by_email('beverley.patterson@environment-agency.gov.uk')
+  admin = Admin.new(:email => 'beverley.patterson@environment-agency.gov.uk', :password => AgencyUser.random_password)
   admin.save!
 end
-if !Admin.find_by_email('admin2@waste-exemplar.gov.uk')
-  admin = Admin.new(:email => 'admin2@waste-exemplar.gov.uk', :password => 'MyS3cr3t!')
-  admin.save!
-end
-if !Admin.find_by_email('michal.sobczak@irmplc.com')
-  admin = Admin.new(:email => 'michal.sobczak@irmplc.com', :password => 'MyS3cr3t!')
-  admin.save!
-end
+#TODO Remove after testing
+#if !Admin.find_by_email('gmueller@caci.co.uk')
+#  admin = Admin.new(:email => 'gmueller@caci.co.uk', :password => AgencyUser.random_password)
+#  admin.save!
+#end
 
-if !AgencyUser.find_by_email('nccc1@waste-exemplar.gov.uk')
-  au = AgencyUser.new(:email => 'nccc1@waste-exemplar.gov.uk', :password => 'secret123')
-  au.save!
-end
+#if !AgencyUser.find_by_email('gmueller@caci.co.uk')
+#  au = AgencyUser.new(:email => 'gmueller@caci.co.uk', :password => AgencyUser.random_password)
+#  au.save!
+#end
 
-if !AgencyUser.find_by_email('nccc2@waste-exemplar.gov.uk')
-  au = AgencyUser.new(:email => 'nccc2@waste-exemplar.gov.uk', :password => 'secret123')
-  au.save!
-end
 
-if (!User.find_by_email('joe@company.com'))
-  user = User.new(:email => 'joe@company.com', :password => 'secret123')
-  user.save!
-end
 
 #Loading agency users from file.
-#data = YAML::load(File.read("db/NCCC-users.txt"))
-#data.split(" ").each {|e| 
-#  if AgencyUser.find_by_email(e)
-#  	puts "AgencyUser with email " + e + " already exists. Not added again."
-#  else
-#    pw = (0...16).map { (65 + SecureRandom.random_number(52)).chr }.join
-#    au = AgencyUser.new(:email => e, :password => pw)
-#    if !au.save
-#      puts "AgencyUser with email " + e + " could not be saved."
-#    end
-#  end
-#}
+data = YAML::load(File.read("db/NCCC-users.txt"))
+data.split(" ").each {|e| 
+  if AgencyUser.find_by_email(e)
+  	puts "AgencyUser with email " + e + " already exists. Not added again."
+  else
+    pw = AgencyUser.random_password
+    au = AgencyUser.new(:email => e, :password => pw)
+    if !au.save
+      puts "AgencyUser with email " + e + " could not be saved."
+    end
+  end
+}
