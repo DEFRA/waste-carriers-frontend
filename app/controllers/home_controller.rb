8fe66a812d8364f92daeee7adf3132ac44940d67
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2013-12-12 17:01:00 +0000

    Replacing static index page with redirect to registrations/find (for Production/Non-Dev only)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
+
   def index
+  	if Rails.env.development?
+  		#redirect_to home_index_path
+  	else
+  		redirect_to find_path
+  	end
   end
+
 end
