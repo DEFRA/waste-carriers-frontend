294184a0c30de760d8d37821018be7a1b66564a5
steven-radford <stevenr@aptosolutions.co.uk>
2014-01-15 15:21:07 +0000

    Update: Add email name to emails such that emails produced by the system include a email name as well as a from address

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,14 +1,17 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "registrations@wastecarriersregistration.service.gov.uk"
   
-  default :from => "\"EA Waste Carriers\" <registrations@wastecarriersregistration.service.gov.uk>"
+  # This line should have set the defailt from name, but didnt in testing,
+  # as such email_with_name variables were created to pass in the email name
+  #default :from => "\"EA Waste Carriers\" <registrations@wastecarriersregistration.service.gov.uk>"
 
   def welcome_email(user, registration)
   	@user = user
   	@url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
   	@registration = registration
+  	email_with_name = "#{config.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
     mail(to: @user.email, subject: 'Waste Carrier Registration Complete',
-    	from: Rails.configuration.registrations_service_email,
+    	from: email_with_name,
     	reply_to: Rails.configuration.registrations_service_email)
   end
   
@@ -16,7 +19,7 @@ class RegistrationMailer < ActionMailer::Base
   	@user = user
   	@url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
   	@registration = registration
-  	email_with_name = "EA Waste Registration <#{Rails.configuration.registrations_service_email}>"
+  	email_with_name = "#{config.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
     mail(to: @user.email, subject: 'The lower tier registration for '+@registration.companyName+' has been revoked. ',
     	from: email_with_name,
     	reply_to: Rails.configuration.registrations_service_email)
