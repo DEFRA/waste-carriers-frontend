9d17445e9dac9f7bad830c1cd31d22f81beb0ca0
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-17 12:26:29 +0100

    Fill in registration form

--- features/step_definitions/lower_tier_registration_steps.rb
+++ features/step_definitions/lower_tier_registration_steps.rb
@@ -1,3 +1,33 @@
 Given(/^I have completed the lower tier registration form$/) do
-  pending
+  visit find_path
+  select 'Sole trader', from: 'What kind of business or organisation are you?'
+  choose 'Only waste from our own business or organisation'
+  choose 'No'
+  click_on 'Continue'
+
+  fill_in 'Business, organisation or trading name', with: 'Grades'
+  click_on 'Next'
+
+  fill_in 'Find address using postcode', with: 'BS1 2EP'
+  click_on 'Find address'
+
+  select 'Grades Gents Hairdressers, 44 Broad Street, City Centre, Bristol BS1 2EP'
+
+  email_address = 'barry@grades.co.uk'
+  select 'Mr', from: 'Title'
+  fill_in 'First name', with: 'Barry'
+  fill_in 'Last name', with: 'Butler'
+  fill_in 'Job title', with: 'Chief Barber'
+  fill_in 'Phone number', with: '0117 926 8332'
+  fill_in 'Email address', with: email_address
+  click_on 'Next'
+
+  check 'By ticking this box I declare'
+  click_on 'Next'
+
+  password = 'password123'
+  fill_in 'Confirm email', with: email_address
+  fill_in 'Create password', with: password
+  fill_in 'Confirm password', with: password
+  click_on 'Next'
 end
