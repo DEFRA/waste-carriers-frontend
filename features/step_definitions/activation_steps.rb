34cdf86129cf7994a2420ffc73b1bce44ff2d19a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-17 12:00:24 +0100

    Generate step definitions by domain

---
+++ features/step_definitions/activation_steps.rb
@@ -0,0 +1,16 @@
+When(/^I activate the account within (\d+) hours$/) do |arg|
+  pending
+end
+
+When(/^I attempt to activate the account after (\d+) hours$/) do |arg|
+  pending
+end
+
+Then(/^my account is successfully activated$/) do
+  pending
+end
+
+Then(/^I need to request a new confirmation email$/) do
+  pending
+end
+
---
+++ features/step_definitions/lower_tier_registration_steps.rb
@@ -0,0 +1,3 @@
+Given(/^I have completed the lower tier registration form$/) do
+  pending
+end
