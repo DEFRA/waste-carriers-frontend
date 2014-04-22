bb16f738a4861c999322a31d598ca6407660d9cc
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-22 09:43:28 +0100

    Convert to RSpec, replace some double-quoted strings, use paths and text on page

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,8 +1,8 @@
 #step definitions for authentication
 
 Given(/^there is an activated user Joe$/) do
-  User.destroy_all(email: 'joe.activated@example.com')
-  user = User.new
+  User.destroy_all(email: 'joe.activated@example.com') # TODO probably don't need to do this now DB is working
+  user = User.new # TODO factory
   user.email = 'joe.activated@example.com'
   user.password = 'secret123'
   #user.send_confirmation_instructions
@@ -12,28 +12,28 @@ Given(/^there is an activated user Joe$/) do
 end
 
 When(/^the user visits the login page$/) do
-  visit '/users/sign_in'
+  visit new_user_session_path
 end
 
 When(/^enters valid credentials for user Joe$/) do
-  assert page.has_content?("Sign in")
-  fill_in "user_email", :with => 'joe.activated@example.com'
-  fill_in "user_password", :with => 'secret123'
-  click_button "Sign in"
+  page.should have_content 'Sign in'
+  fill_in 'Email', with: 'joe.activated@example.com'
+  fill_in 'Password', with: 'secret123'
+  click_button 'Sign in'
 end
 
 Then(/^the user should be logged in successfully$/) do
-  assert page.has_content?("Signed in as")
+  page.should have_content 'Signed in as'
 end
 
 When(/^enters invalid credentials for user Joe$/) do
-  assert page.has_content?("Sign in")
-  fill_in "user_email", :with => 'joe.activated@example.com'
-  fill_in "user_password", :with => 'secret234'
-  click_button "Sign in"
+  page.should have_content 'Sign in'
+  fill_in 'Email', with: 'joe.activated@example.com'
+  fill_in 'Password', with: 'secret234'
+  click_button 'Sign in'
 end
 
 Then(/^the user should see a login error$/) do
-  assert page.has_content?("Invalid email or password.")
+  page.should have_content 'Invalid email or password.'
 end
 
