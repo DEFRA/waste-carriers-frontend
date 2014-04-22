a615c05ebe022afe01acdc09ecf361d101ae5683
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-22 09:45:47 +0100

    Convert to RSpec

--- features/step_definitions/find_out_if_need_to_register_steps.rb
+++ features/step_definitions/find_out_if_need_to_register_steps.rb
@@ -1,14 +1,14 @@
 ## step definitions for 'Find out if I need to register'
 
 Given(/^I have found out that I need to register in the lower tier$/) do
-  visit '/registrations/find'
-  assert page.has_content?("Find out if you need to register and which category applies")
-  page.select('Sole trader', :from => 'discover_businessType')
+  visit find_path
+  page.should have_content 'Find out if you need to register and which category applies'
+  page.select 'Sole trader', from: 'discover_businessType'
   choose 'discover_otherBusinesses_no'
   choose 'discover_constructionWaste_no'
   #check 'discover_wasteType_other'
   #within("//div[@id='lowerText']") do
   #  click_button "Continue >"
   #end
-  click_button "register_lower_tier"
+  click_button 'register_lower_tier'
 end
