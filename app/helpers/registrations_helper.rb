dd0b1ed004ce19ac09c0552cfee11ec32f9d4b6a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-22 15:20:04 +0100

    Avert potential cross-site scripting (XSS). The code was protected before but only because it disallowed certain characters in the input which is a different concern.

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -24,14 +24,14 @@ end
 def format_address(model)
   if model.postcode.nil?
     # Print International address
-    raw("#{model.streetLine1}<br />#{model.streetLine2}<br />#{model.streetLine3}<br />#{model.streetLine4}<br />#{model.country}")
+    "#{h(model.streetLine1)}<br />#{h(model.streetLine2)}<br />#{h(model.streetLine3)}<br />#{h(model.streetLine4)}<br />#{h(model.country)}".html_safe
   else
     if model.streetLine2 != ""
       # Print UK Address Including Street line 2 (as its optional but been populated)
-      raw("#{model.houseNumber} #{model.streetLine1}<br />#{model.streetLine2}<br />#{model.townCity}<br />#{model.postcode}")
+      "#{h(model.houseNumber)} #{h(model.streetLine1)}<br />#{h(model.streetLine2)}<br />#{h(model.townCity)}<br />#{h(model.postcode)}".html_safe
     else
       # Print UK Address
-      raw("#{model.houseNumber} #{model.streetLine1}<br />#{model.townCity}<br />#{model.postcode}")
+      "#{h(model.houseNumber)} #{h(model.streetLine1)}<br />#{h(model.townCity)}<br />#{h(model.postcode)}".html_safe
     end    
   end
 end
