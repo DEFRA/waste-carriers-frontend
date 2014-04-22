4657b37a1863be126f54c4d21b3f0e6df00fa9ad
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-22 13:11:50 +0100

    Remove comments

--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -24,15 +24,6 @@ Then(/^I click "(.*?)"$/) do |name|
 end
 
 Then(/^I fill in valid contact details$/) do
-
-  ## With address lookup enabled and working, users do not enter house number etc. anymore
-  #find_field('registration_houseNumber')
-  ##assert(page.has_content?('Address and contact details'))
-  #fill_in('registration_houseNumber', :with => '12')
-  #fill_in('registration_streetLine1', :with => 'Deanery Road')
-  #fill_in('registration_townCity', :with => 'Bristol')
-  #fill_in('registration_postcode', :with => 'BS1 5AH')
-
   fill_in 'sPostcode', with: 'BS1 5AH'
   click_button 'Find address'
   page.select "Environment Agency, Horizon House, Deanery Road, City Centre, Bristol BS1 5AH", from: 'sSelect'
@@ -59,11 +50,7 @@ Then(/^I should see the Confirm Account page$/) do
   page.should have_content 'Follow the instructions in the email to confirm your account and complete your registration'
 end
 
-#Alternate version....
-
-
 When(/^I provide valid individual trading name details$/) do
-  #page.select('Sole trader', :from => 'registration_businessType')
   fill_in 'registration_companyName', with: 'Joe Bloggs'
   click_on 'Next'
 end
@@ -75,13 +62,6 @@ When(/^I provide valid individual trading name details including business type$/
 end
 
 When(/^I provide valid contact details$/) do
-  #find_field('registration_houseNumber')
-  ##assert(page.has_content?('Address and contact details'))
-  #fill_in('registration_houseNumber', :with => '12')
-  #fill_in('registration_streetLine1', :with => 'Some Road')
-  #fill_in('registration_townCity', :with => 'Some Town')
-  #fill_in('registration_postcode', :with => 'SW12 3AB')
-
   fill_in 'sPostcode', with: 'BS1 5AH'
   click_button 'Find address'
   page.select "Environment Agency, Horizon House, Deanery Road, City Centre, Bristol BS1 5AH", from: 'sSelect'
@@ -92,16 +72,9 @@ When(/^I provide valid contact details$/) do
   fill_in 'registration_phoneNumber', with: '0123 456'
   fill_in 'registration_contactEmail', with: 'joe.bloggs@example.com'
   click_on 'Next'
-  #assert(page.has_content?('Check details and register'))
 end
 
 Given(/^I provide valid contact details for "(.*?)"$/) do |email|
-  #find_field('registration_houseNumber')
-  ##assert(page.has_content?('Address and contact details'))
-  #fill_in('registration_houseNumber', :with => '12')
-  #fill_in('registration_streetLine1', :with => 'Some Road')
-  #fill_in('registration_townCity', :with => 'Some Town')
-  #fill_in('registration_postcode', :with => 'SW12 3AB')
   fill_in 'sPostcode', with: 'BS1 5AH'
   click_button 'Find address'
   page.select "Environment Agency, Horizon House, Deanery Road, City Centre, Bristol BS1 5AH", from: 'sSelect'
@@ -112,7 +85,6 @@ Given(/^I provide valid contact details for "(.*?)"$/) do |email|
   fill_in 'registration_phoneNumber', with: '0123 456'
   fill_in 'registration_contactEmail', with: email
   click_on 'Next'
-  #assert(page.has_content?('Check details and register'))
 end
 
 When(/^I confirm the declaration$/) do
@@ -122,13 +94,11 @@ When(/^I confirm the declaration$/) do
 end
 
 When(/^I provide valid user details for sign up$/) do
-  #page.select('Sign up (new e-mail)', :from => 'registration_sign_up_mode')
   unique_email = 'test-' + SecureRandom.urlsafe_base64 + '@example.com' # TODO factory
   fill_in 'registration_accountEmail', with: unique_email
   fill_in 'registration_accountEmail_confirmation', with: unique_email
   fill_in 'registration_password', with: 'Bloggs123!'
   fill_in 'registration_password_confirmation', with: 'Bloggs123!'
-  #click_on 'Register'
 end
 
 
@@ -176,8 +146,6 @@ Given(/^I have an activated account$/) do
   user.email = theUsersEmail
   user.password = theUsersPassword
   user.password_confirmation = theUsersPassword
-  #user.send_confirmation_instructions
-  #user.confirm!
   user.skip_confirmation!
   user.save!
 
@@ -211,15 +179,11 @@ Given(/^I am already logged in$/) do
 end
 
 When(/^proceed to the Address and Contact Details page$/) do
-  #page.select('Sole trader', :from => 'registration_businessType')
   fill_in 'registration_companyName', with: 'Joe Bloggs'
   click_on 'Next'
 end
 
 When(/^I prepare to enter an address manually$/) do
-  ## At least at the moment the user has to perform a search in order to make the link appear
-  #fill_in('sPostcode', :with => 'BS1 5AH')
-  #click_button 'Find address'
   click_link 'I want to add an address myself'
 end
 
@@ -237,10 +201,6 @@ Then(/^when I click on the activation link$/) do
   open_email User.last.email
   current_email.click_link 'Confirm your account'
 end
-
-#Then(/^my registration should be activated$/) do
-#  page.should have_content("You are now registered as a lower tier waste carrier/broker/dealer with the Environment Agency.")
-#end
 
 Then(/^I should see the Registration Confirmed page$/) do
   page.should have_content "Registration complete"
