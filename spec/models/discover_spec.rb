ae6d84b3e5a478579d27e28dda11a9e0c7db4c12
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-24 16:30:28 +0100

    Spec up method

--- spec/models/discover_spec.rb
+++ spec/models/discover_spec.rb
@@ -3,8 +3,16 @@ require 'spec_helper'
 describe Discover do
 
   describe "#isUpperBusinessType?" do
+    specify { Discover.new(businessType: 'soleTrader').isUpperBusinessType? == true }
+    specify { Discover.new(businessType: 'partnership').isUpperBusinessType? == true }
+    specify { Discover.new(businessType: 'limitedCompany').isUpperBusinessType? == true }
+    specify { Discover.new(businessType: 'publicBody').isUpperBusinessType? == true }
 
-
+    specify { Discover.new(businessType: 'charity').isUpperBusinessType? == false }
+    specify { Discover.new(businessType: 'collectionAuthority').isUpperBusinessType? == false }
+    specify { Discover.new(businessType: 'disposalAuthority').isUpperBusinessType? == false }
+    specify { Discover.new(businessType: 'regulationAuthority').isUpperBusinessType? == false }
+    specify { Discover.new(businessType: 'other').isUpperBusinessType? == false }
   end
 
   describe "#is_upper?" do
