95f23d055dc6b6d342b18b42bca4f814d40feaa2
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-04-24 16:38:42 +0100

    Rename method to follow Ruby conventions

--- spec/models/discover_spec.rb
+++ spec/models/discover_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 
 describe Discover do
 
-  describe "#isUpperBusinessType?" do
-    specify { Discover.new(businessType: 'soleTrader').isUpperBusinessType? == true }
-    specify { Discover.new(businessType: 'partnership').isUpperBusinessType? == true }
-    specify { Discover.new(businessType: 'limitedCompany').isUpperBusinessType? == true }
-    specify { Discover.new(businessType: 'publicBody').isUpperBusinessType? == true }
+  describe "#upper_business_type?" do
+    specify { Discover.new(businessType: 'soleTrader').upper_business_type? == true }
+    specify { Discover.new(businessType: 'partnership').upper_business_type? == true }
+    specify { Discover.new(businessType: 'limitedCompany').upper_business_type? == true }
+    specify { Discover.new(businessType: 'publicBody').upper_business_type? == true }
 
-    specify { Discover.new(businessType: 'charity').isUpperBusinessType? == false }
-    specify { Discover.new(businessType: 'collectionAuthority').isUpperBusinessType? == false }
-    specify { Discover.new(businessType: 'disposalAuthority').isUpperBusinessType? == false }
-    specify { Discover.new(businessType: 'regulationAuthority').isUpperBusinessType? == false }
-    specify { Discover.new(businessType: 'other').isUpperBusinessType? == false }
+    specify { Discover.new(businessType: 'charity').upper_business_type? == false }
+    specify { Discover.new(businessType: 'collectionAuthority').upper_business_type? == false }
+    specify { Discover.new(businessType: 'disposalAuthority').upper_business_type? == false }
+    specify { Discover.new(businessType: 'regulationAuthority').upper_business_type? == false }
+    specify { Discover.new(businessType: 'other').upper_business_type? == false }
 
-    specify { Discover.new(businessType: '').isUpperBusinessType? == false }
-    specify { Discover.new(businessType: nil).isUpperBusinessType? == false }
+    specify { Discover.new(businessType: '').upper_business_type? == false }
+    specify { Discover.new(businessType: nil).upper_business_type? == false }
   end
 
   describe "#is_upper?" do
