5e1e37da09a4bbfaca82963e47c18987aaa81d46
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-05-02 16:17:48 +0100

    Corrected actions inline with PO's current wireframes

--- app/controllers/upper_registrations_controller.rb
+++ app/controllers/upper_registrations_controller.rb
@@ -1,31 +1,27 @@
 class UpperRegistrationsController < ApplicationController
 
-  def business_name
+  def business_details
     @registration = UpperRegistration.new
     session[:upper_reg_params] ||= {}
     session[:upper_reg_params].deep_merge!(upper_reg_params) if params[:upper_registration]
 
     @registration = UpperRegistration.new(session[:upper_reg_params])
-    @registration.current_step = "business"
+    @registration.current_step = "business_details"
   end
 
-  def business_name_update
+  def business_details_update
     session[:upper_reg_params] ||= {}
     session[:upper_reg_params].deep_merge!(upper_reg_params) if params[:upper_registration]
 
     @registration = UpperRegistration.new(session[:upper_reg_params])
-    @registration.current_step = "business"
+    @registration.current_step = "business_details"
 
     if @registration.valid?
       logger.info "GOT A VALID REGISTRATION"
-      redirect_to :upper_business_name
+      redirect_to :upper_business_details
     elsif @registration.new_record?
-      redirect_to :upper_business_name
+      redirect_to :upper_business_details
     end
-  end
-
-  def main_address
-
   end
 
   def director_address
