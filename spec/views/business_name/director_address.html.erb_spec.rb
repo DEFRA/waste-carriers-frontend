554122886bfbc0c07256526701b04ea8609a799f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-05-02 15:34:44 +0100

    Started work on prototype upper tier pages [#70425718]

---
+++ spec/views/business_name/director_address.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "business_name/director_address.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
---
+++ spec/views/upper_registrations/main_address.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe "upper_registrations/main_address.html.erb" do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
