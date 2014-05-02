9d5fd2ac0cf482a62b5f215ff9ab4e7bebdc8b93
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-05-02 16:09:58 +0100

    Got a working first page that posts back valid data

--- app/models/upper_registration.rb
+++ app/models/upper_registration.rb
@@ -1,8 +1,11 @@
 require 'active_resource'
 
-class UpperRegistration < ActiveResource::Base
+class UpperRegistration
+  include ActiveModel::Model
 
   attr_accessor :business_name, :carrier, :broker, :dealer
   attr_writer :current_step
+
+  validates_presence_of :business_name
 
 end
