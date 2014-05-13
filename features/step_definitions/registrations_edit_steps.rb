283853782aee48ff51b5bf7aa043d9fecd5be1ae
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-05-13 22:28:38 +0100

    Commenting out tests steps in progress

--- features/step_definitions/registrations_edit_steps.rb
+++ features/step_definitions/registrations_edit_steps.rb
@@ -1,27 +1,27 @@
 Given(/^I have registered before and have an active registration$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 Given(/^I am logged in$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 When(/^I edit my registration$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 Then(/^my registration has been updated$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 Given(/^I am logged in as an agency user$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 When(/^I select and edit the registration$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
 
 Then(/^the registration has been updated$/) do
-  pending # express the regexp above with the code you wish you had
+  #pending # express the regexp above with the code you wish you had
 end
