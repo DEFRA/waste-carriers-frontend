2032624ce4eb57dced2bf1926953b0ea01dfbe89
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-13 15:06:41 +0100

    Add to steps but need to think about how to do this

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -162,7 +162,7 @@ class Registration < ActiveResource::Base
   end
 
   def steps
-    %w[businesstype otherbusinesses serviceprovided business contact confirmation signup]
+    %w[businesstype otherbusinesses serviceprovided constructiondemolition business contact confirmation signup]
   end
 
   VALID_SIGN_UP_MODES = %w[sign_up sign_in]
