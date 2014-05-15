7dd544ed4ee90d72b37c5e082dcfc2dcd46104f3
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-15 16:02:52 +0100

    Make these choose from radio buttons

--- features/step_definitions/find_out_if_need_to_register_steps.rb
+++ features/step_definitions/find_out_if_need_to_register_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I have found out that I need to register in the lower tier$/) do
   visit find_path
-  page.select 'Sole trader', from: 'registration_businessType'
+  choose 'Sole trader'
   click_on 'Next'
   choose 'registration_otherBusinesses_no'
   click_on 'Next'
