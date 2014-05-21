a9e37ac32fabe2d37cec4f8ea4953a81b53e721b
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-21 09:29:20 +0100

    Fill in first steps

--- features/step_definitions/discovery_steps.rb
+++ features/step_definitions/discovery_steps.rb
@@ -1,3 +1,8 @@
 Given(/^I am on the discovery page$/) do
-  pending
+  visit find_path
+end
+
+And(/^I enter my business type as (.*)$/) do |business_type|
+  choose "registration_businessType_#{business_type}"
+  click_on 'Next'
 end
