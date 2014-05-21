20b5a915ba2042a835a0e9bb63baf9d0d5b3107d
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-21 11:12:46 +0100

    Some more cukes passing

--- features/step_definitions/discovery_steps.rb
+++ features/step_definitions/discovery_steps.rb
@@ -39,3 +39,8 @@ And(/^I indicate disposing waste is my main service$/) do
   choose 'registration_isMainService_yes'
   click_on 'Next'
 end
+
+And(/^I indicate I only deal with animal, farm, quarry or mine waste$/) do
+  choose 'registration_onlyAMF_yes'
+  click_on 'Next'
+end
