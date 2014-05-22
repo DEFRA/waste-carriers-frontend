e7c0e56c2db9b5dae731604808b4be9ab9dc9802
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-22 11:33:05 +0100

    Make these specs pending for now while validations have been turned off

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -4,8 +4,10 @@ describe Registration do
   context 'businesstype step' do
     subject { Registration.new(current_step: 'businesstype') }
 
-    it { should validate_presence_of(:businessType).with_message(/must be completed/) }
-    it { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'collectionAuthority', 'disposalAuthority', 'regulationAuthority', 'other').for(:businessType) }
-    it { should_not allow_value('sole_trader', 'plc').for(:businessType) }
+    # TODO need to put validation back in after show-and-tell
+
+    xit { should validate_presence_of(:businessType).with_message(/must be completed/) }
+    xit { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'collectionAuthority', 'disposalAuthority', 'regulationAuthority', 'other').for(:businessType) }
+    xit { should_not allow_value('sole_trader', 'plc').for(:businessType) }
   end
 end
