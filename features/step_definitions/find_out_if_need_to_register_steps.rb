3e9c55a57e8482bfc9c14bb594e24e3d840fa038
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-28 11:42:43 +0100

    Remove unused steps

--- features/step_definitions/find_out_if_need_to_register_steps.rb
+++
@@ -1,9 +0,0 @@
-Given(/^I have found out that I need to register in the lower tier$/) do
-  visit find_path
-  choose 'Sole trader'
-  click_on 'Next'
-  choose 'registration_otherBusinesses_no'
-  click_on 'Next'
-  choose 'registration_constructionWaste_no'
-  click_on 'Next'
-end
