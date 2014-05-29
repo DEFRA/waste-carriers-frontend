4cb333f923f72602825dd329c7f2db9f10047b0f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-29 15:16:01 +0100

    Add spec that doesn't use shoulda to see if proves anything

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -8,4 +8,30 @@ describe Registration do
     it { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'authority', 'other').for(:businessType) }
     it { should_not allow_value('sole_trader', 'plc', 'collectionAuthority', 'disposalAuthority', 'regulationAuthority').for(:businessType) }
   end
+
+  context 'otherbusinesses step' do
+    before do
+      @registration = Registration.new(current_step: 'otherbusinesses')
+    end
+
+    subject { @registration }
+
+    it { should_not be_valid }
+
+    describe 'with otherbusinesses yes' do
+      before do
+        @registration.otherBusinesses = 'yes'
+      end
+
+      it { should be_valid }
+    end
+
+    describe 'with otherbusinesses y' do
+      before do
+        @registration.otherBusinesses = 'y'
+      end
+
+      it { should_not be_valid }
+    end
+  end
 end
