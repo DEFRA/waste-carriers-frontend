5ef21e29e16abf01485cb3a8e11069cc67e2e5a3
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-29 15:34:28 +0100

    This is the default step anyway so take out

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Registration do
   context 'businesstype step' do
-    subject { Registration.new(current_step: 'businesstype') }
+    subject { Registration.new }
 
     it { should validate_presence_of(:businessType).with_message(/must be completed/) }
     it { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'authority', 'other').for(:businessType) }
