4386d62c03cef89e3f76f319784f7c1b70eace06
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-29 10:45:19 +0100

    Spec passing. Also showing what's happened with authorities

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -4,10 +4,8 @@ describe Registration do
   context 'businesstype step' do
     subject { Registration.new(current_step: 'businesstype') }
 
-    # TODO need to put validation back in after show-and-tell
-
-    xit { should validate_presence_of(:businessType).with_message(/must be completed/) }
-    xit { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'collectionAuthority', 'disposalAuthority', 'regulationAuthority', 'other').for(:businessType) }
-    xit { should_not allow_value('sole_trader', 'plc').for(:businessType) }
+    it { should validate_presence_of(:businessType).with_message(/must be completed/) }
+    it { should allow_value('soleTrader', 'partnership', 'limitedCompany', 'publicBody', 'charity', 'authority', 'other').for(:businessType) }
+    it { should_not allow_value('sole_trader', 'plc', 'collectionAuthority', 'disposalAuthority', 'regulationAuthority').for(:businessType) }
   end
 end
