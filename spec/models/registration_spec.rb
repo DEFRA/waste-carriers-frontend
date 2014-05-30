e97fbe988492339a09e67b8b2e59cfe63bcd1aed
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-05-30 09:42:35 +0100

    Add passing spec

--- spec/models/registration_spec.rb
+++ spec/models/registration_spec.rb
@@ -38,4 +38,14 @@ describe Registration do
     it { should allow_value('yes', 'no').for(:constructionWaste) }
     it { should_not allow_value('y', 'n').for(:constructionWaste) }
   end
+
+  context 'onlydealwith step' do
+    subject { Registration.new }
+
+    before { subject.current_step = 'onlydealwith' }
+
+    it { should validate_presence_of(:onlyAMF).with_message(/must be completed/) }
+    it { should allow_value('yes', 'no').for(:onlyAMF) }
+    it { should_not allow_value('y', 'n').for(:onlyAMF) }
+  end
 end
