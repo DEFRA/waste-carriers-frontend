a3f8850120c0ac723279be3f1dc86ed762ba6635
Stinkypants <alan.cruikshanks@gmail.com>
2014-06-02 23:49:19 +0100

    Chk box and labels on same line, plus making proper use of new report model for validation.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,28 +6,41 @@ class ReportsController < ApplicationController
 
   # GET /report/registrations
   def reportRegistrations
-    new_action
-    fromParam = params[:from]
-    untilParam = params[:until]
-    @registrations = []
-    @hasErrors = false
-    if !fromParam.nil? and !untilParam.nil?
-      if fromParam =~ /^\d\d\/\d\d\/\d\d\d\d$/ and untilParam =~ /^\d\d\/\d\d\/\d\d\d\d$/
-      @registrations = Registration.find(:all, :params => {:from => fromParam, :until => untilParam, :route => params[:route], :status => params[:status], :businessType => params[:businessType], :ac => params[:email]})
-    else
-      @hasErrors = true
-      if request.format == 'csv'
-          redirect_to reports_registrations_path(format: 'html', from: fromParam, until: untilParam, route: params[:route], status: params[:status], businessType: params[:businessType] )
-        end
-    end
-    end
-    if !@hasErrors
-      respond_to do |format|
-        format.json { render json: @registrations }
-        format.csv
-        format.html
+
+    @report = Report.new(params[:report])
+
+    unless @report.is_new.blank?
+      @report.is_new = 'false'
+
+      if @report.valid?
+        render "reportRegistrations"
+      else
+        logger.info 'Report filters are not valid'
+        render "reportRegistrations", :status => '400'
       end
     end
+
+    # fromParam = params[:from]
+    # untilParam = params[:until]
+    # @registrations = []
+    # @hasErrors = false
+    # if !fromParam.nil? and !untilParam.nil?
+    #   if fromParam =~ /^\d\d\/\d\d\/\d\d\d\d$/ and untilParam =~ /^\d\d\/\d\d\/\d\d\d\d$/
+    #   @registrations = Registration.find(:all, :params => {:from => fromParam, :until => untilParam, :route => params[:route], :status => params[:status], :businessType => params[:businessType], :ac => params[:email]})
+    # else
+    #   @hasErrors = true
+    #   if request.format == 'csv'
+    #       redirect_to reports_registrations_path(format: 'html', from: fromParam, until: untilParam, route: params[:route], status: params[:status], businessType: params[:businessType] )
+    #     end
+    # end
+    # end
+    # if !@hasErrors
+    #   respond_to do |format|
+    #     format.json { render json: @registrations }
+    #     format.csv
+    #     format.html
+    #   end
+    # end
   end
 
   # TODO Helper/Shared functions which should be refactored out
