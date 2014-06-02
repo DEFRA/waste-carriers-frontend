8d26eb46da9c68658f6fa34e0526d368a3f3db63
Stinkypants <alan.cruikshanks@gmail.com>
2014-06-02 23:49:19 +0100

    Chk box and labels on same line, plus making proper use of new report model for validation.

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,35 @@ require 'active_resource'
 class Report
   include ActiveModel::Model
 
-  attr_accessor :from, :to, :route_digital, :route_assisted_digital
+  attr_accessor :is_new, :from, :to, :route_digital, :route_assisted_digital
+
+  validate :validate_from
+  validate :validate_to
+
+  private
+
+    def validate_from
+
+      if from.blank?
+        Rails.logger.debug "report 'from' field is empty"
+        errors.add(:from, I18n.t('errors.messages.blank') )
+      elsif from =~ /^\d\d\/\d\d\/\d\d\d\d$/
+        Rails.logger.debug "report 'from' field is invalid"
+        errors.add(:from, I18n.t('errors.messages.invalid') )
+      end
+
+    end
+
+    def validate_to
+
+      if to.blank?
+        Rails.logger.debug "report 'to' field is empty"
+        errors.add(:to, I18n.t('errors.messages.blank') )
+      elsif to =~ /^\d\d\/\d\d\/\d\d\d\d$/
+        Rails.logger.debug "report 'to' field is invalid"
+        errors.add(:to, I18n.t('errors.messages.invalid') )
+      end
+
+    end
 
 end
