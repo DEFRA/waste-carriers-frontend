83563c19d6223bde23d08982975be2774964e05f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-06-12 17:25:55 +0100

    Pad with zeroes

--- spec/models/companies_house_caller_spec.rb
+++ spec/models/companies_house_caller_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe CompaniesHouseCaller do
-  describe 'call' do
-    subject { CompaniesHouseCaller.new '02050399' }
+  describe 'url' do
+    context '8 characters' do
+      subject { CompaniesHouseCaller.new '02050399' }
 
-    its(:url) { should == 'http://data.companieshouse.gov.uk/doc/company/02050399.json' }
+      its(:url) { should == 'http://data.companieshouse.gov.uk/doc/company/02050399.json' }
+    end
+
+    context 'fewer than 8 characters' do
+      subject { CompaniesHouseCaller.new '2050399' }
+
+      its(:url) { should == 'http://data.companieshouse.gov.uk/doc/company/02050399.json' }
+    end
   end
 end
