97bffd2621ea3ba29e8141d4d976d97bc4ebf104
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-06-12 17:28:32 +0100

    Extract constant

--- app/models/companies_house_caller.rb
+++ app/models/companies_house_caller.rb
@@ -1,8 +1,10 @@
 class CompaniesHouseCaller
   attr_reader :url
 
+  @@COMPANIES_HOUSE_URL = 'http://data.companieshouse.gov.uk/doc/company/'
+
   def initialize companies_house_registration_number
-    @url = "http://data.companieshouse.gov.uk/doc/company/#{pad_with_zeroes_to_make_it_eight_characters companies_house_registration_number}.json"
+    @url = "#{@@COMPANIES_HOUSE_URL}#{pad_with_zeroes_to_make_it_eight_characters companies_house_registration_number}.json"
   end
 
 private
