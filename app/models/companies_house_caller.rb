7d4e00685a11943bfce17895c4fb4d44ea0dd07f
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-06-12 17:27:19 +0100

    Refactor

--- app/models/companies_house_caller.rb
+++ app/models/companies_house_caller.rb
@@ -2,7 +2,12 @@ class CompaniesHouseCaller
   attr_reader :url
 
   def initialize companies_house_registration_number
-    companies_house_registration_number_padded_with_zeroes_to_eight_characters = companies_house_registration_number.rjust(8, '0')
-    @url = "http://data.companieshouse.gov.uk/doc/company/#{companies_house_registration_number_padded_with_zeroes_to_eight_characters}.json"
+    @url = "http://data.companieshouse.gov.uk/doc/company/#{pad_with_zeroes_to_make_it_eight_characters companies_house_registration_number}.json"
+  end
+
+private
+
+  def pad_with_zeroes_to_make_it_eight_characters companies_house_registration_number
+    companies_house_registration_number.rjust(8, '0')
   end
 end
