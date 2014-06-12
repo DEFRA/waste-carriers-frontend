1f63d8b6f804a9075b777f564328b2780139ee4e
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-06-12 17:25:55 +0100

    Pad with zeroes

--- app/models/companies_house_caller.rb
+++ app/models/companies_house_caller.rb
@@ -2,6 +2,7 @@ class CompaniesHouseCaller
   attr_reader :url
 
   def initialize companies_house_registration_number
-    @url = "http://data.companieshouse.gov.uk/doc/company/#{companies_house_registration_number}.json"
+    companies_house_registration_number_padded_with_zeroes_to_eight_characters = companies_house_registration_number.rjust(8, '0')
+    @url = "http://data.companieshouse.gov.uk/doc/company/#{companies_house_registration_number_padded_with_zeroes_to_eight_characters}.json"
   end
 end
