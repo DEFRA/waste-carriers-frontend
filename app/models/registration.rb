7b2a0df9ea09021503022e01e571b2b326bb1613
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-06-17 14:13:31 +0100

    Add method

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -119,7 +119,7 @@ class Registration < ActiveResource::Base
 
   validates :companyName, presence: true, format: { with: VALID_COMPANY_NAME_REGEX, message: I18n.t('errors.messages.alpha70') }, length: { maximum: 70 }, if: 'businessdetails_step? or upperbusinessdetails_step?'
 
-  with_options if: 'contactdetails_step? or uppercontactdetails_step?' do |registration|
+  with_options if: :lower_or_upper_contact_details_step? do |registration|
     registration.validates :firstName, presence: true, format: { with: GENERAL_WORD_REGEX }, length: { maximum: 35 }
     registration.validates :lastName, presence: true, format: { with: GENERAL_WORD_REGEX }, length: { maximum: 35 }
     registration.validates :position, presence: true, format: { with: GENERAL_WORD_REGEX }
@@ -212,6 +212,10 @@ class Registration < ActiveResource::Base
 
   def upperbusinessdetails_step?
     current_step.inquiry.upper_business_details?
+  end
+
+  def lower_or_upper_contact_details_step?
+    contactdetails_step? or uppercontactdetails_step?
   end
 
   def contactdetails_step?
