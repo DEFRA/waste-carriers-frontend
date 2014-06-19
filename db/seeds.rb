ca56ef40098d2c0f137cef618fbfd478094a8d9f
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-06-19 15:17:50 +0100

    Preparing for publishing code - removing non-public data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,31 +13,6 @@ if !Rails.env.production?
   end
 end
 
-if !Admin.find_by_email('jamie.dempster@environment-agency.gov.uk')
-  admin = Admin.new(:email => 'jamie.dempster@environment-agency.gov.uk', :password => AgencyUser.random_password)
-  admin.save!
-end
-if !Admin.find_by_email('beverley.patterson@environment-agency.gov.uk')
-  admin = Admin.new(:email => 'beverley.patterson@environment-agency.gov.uk', :password => AgencyUser.random_password)
-  admin.save!
-end
-if !Admin.find_by_email("dermot.o'regan@environment-agency.gov.uk")
-  admin = Admin.new(:email => "dermot.o'regan@environment-agency.gov.uk", :password => AgencyUser.random_password)
-  admin.save!
-end
-
-#TODO Remove after testing
-#if !Admin.find_by_email('gmueller@caci.co.uk')
-#  admin = Admin.new(:email => 'gmueller@caci.co.uk', :password => AgencyUser.random_password)
-#  admin.save!
-#end
-
-#if !AgencyUser.find_by_email('gmueller@caci.co.uk')
-#  au = AgencyUser.new(:email => 'gmueller@caci.co.uk', :password => AgencyUser.random_password)
-#  au.save!
-#end
-
-
 
 #Loading agency users from file.
 data = YAML::load(File.read("db/NCCC-Users.txt"))
