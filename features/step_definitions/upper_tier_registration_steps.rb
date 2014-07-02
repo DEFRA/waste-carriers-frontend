034378f582911c1095f2151a0b056419f8b265d5
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-02 11:46:08 +0100

    Add scenarios and make start on step

--- features/step_definitions/upper_tier_registration_steps.rb
+++ features/step_definitions/upper_tier_registration_steps.rb
@@ -25,3 +25,26 @@ Then(/^I am registered as an upper tier waste carrier$/) do
   open_email my_email_address
   current_email.should have_content 'is registered as an upper tier waste carrier'
 end
+
+And(/^I pay by card$/) do
+  click_on 'Pay by debit/credit card'
+
+  click_on 'MasterCard'
+
+  fill_in 'Card number', with: '4444333322221111'
+  select '12', from: 'cardExp.month'
+  select Date.current.year + 2, from: 'cardExp.year'
+  fill_in "Cardholder's name", with: 'B Butler'
+  fill_in 'Address 1', with: 'Deanery St.'
+  fill_in 'Town/City', with: 'Bristol'
+  fill_in 'Postcode/ZIP', with: 'BS1 5AH'
+  click_on 'op-PMMakePayment'
+
+  sleep 1.5
+  click_on 'Continue'
+
+  # click through temporary page but eventually this page should disappear
+  # click_on 'Continue'
+
+  save_and_open_page
+end
