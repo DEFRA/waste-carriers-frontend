a154f0466d905945fa95a0b9c6e885dd95e195a3
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-09 17:29:25 +0100

    Make start on cancan specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,24 @@
 require 'spec_helper'
+require 'cancan/matchers'
 
 describe User do
+  describe 'abilities' do
+    subject(:ability) { Ability.new(user) }
+    let(:user) { nil }
+
+    context 'waste carrier user' do
+      let(:user) { Factory(:user) }
+
+      it{ should be_able_to(:x, Registration.new) }
+    end
+
+    context 'NCCC agency user' do
+      let(:user) { Factory(:agency_user) }
+
+      it{ should be_able_to(:x, Registration.new) }
+    end
+  end
+
   describe '#confirmed?' do
     context 'missing confirmed_at' do
       before do
