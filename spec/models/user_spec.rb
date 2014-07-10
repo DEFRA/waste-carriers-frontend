afbc2ceef7d0d545c693aacb1052494f68e77e1a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-10 15:27:29 +0100

    Specs finally pass, problem was to do with mocking

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,16 +4,21 @@ require 'cancan/matchers'
 describe User do
   describe 'abilities' do
     context 'waste carrier user' do
+      let(:user) { FactoryGirl.create :user }
+
       context 'own certificate' do
-        let(:user) { FactoryGirl.create :user }
-        let(:registration) { double(user: user) }
+        let(:registration) { Registration.new }
         subject(:ability) { Ability.new(user) }
+
+        before do
+          allow(registration).to receive(:user).and_return(user)
+          allow(registration).to receive(:tier).and_return('LOWER')
+        end
 
         it { should be_able_to(:print, registration) }
       end
 
       context 'certificate for other waste carrier' do
-        let(:user) { FactoryGirl.create :user }
         let(:other_user) { FactoryGirl.build :user }
         let(:registration) { double(user: other_user) }
         subject(:ability) { Ability.new(user) }
@@ -23,15 +28,19 @@ describe User do
 
       context 'upper tier' do
         context 'paid' do
-          let(:user) { FactoryGirl.create :user }
-          let(:registration) { double(user: user, paid_in_full?: true, tier: 'UPPER' ) }
+          let(:registration) { Registration.new }
           subject(:ability) { Ability.new(user) }
+
+          before do
+            allow(registration).to receive(:user).and_return(user)
+            allow(registration).to receive(:paid_in_full?).and_return(true)
+            allow(registration).to receive(:tier).and_return('UPPER')
+          end
 
           it { should be_able_to(:print, registration) }
         end
 
         context 'unpaid' do
-          let(:user) { FactoryGirl.create :user }
           let(:registration) { double(user: user, paid_in_full?: false, tier: 'UPPER' ) }
           subject(:ability) { Ability.new(user) }
 
