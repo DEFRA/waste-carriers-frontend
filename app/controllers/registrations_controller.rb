1fe6acccd0ced2a982222958d071bc513a8de08f
steven-radford <stevenr@aptosolutions.co.uk>
2014-07-10 17:13:35 +0100

    Apply more accurate Payment permissions

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -474,6 +474,7 @@ class RegistrationsController < ApplicationController
     @registration = Registration.find(:one, :from => "/registrations/"+params[:id]+".json")
     @registration.routeName = @registration.metaData.route
     authorize! :read, @registration
+    authorize! :read, Payment
   end
 
   def check_steps_are_valid_up_until_current current_step
