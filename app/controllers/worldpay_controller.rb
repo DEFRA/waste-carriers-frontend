a508ff44241e6590211e62538c7af7841a4ded85
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-07-11 23:06:53 +0100

    Attempting to make the registration flow work with Redis

--- app/controllers/worldpay_controller.rb
+++ app/controllers/worldpay_controller.rb
@@ -7,7 +7,7 @@ class WorldpayController < ApplicationController
   def success
     #TODO - redirect to some other page after processing/saving the payment
     #redirect_to paid_path
-    @registration = Registration.find session[:registration_id]
+    @registration = Registration[session[:registration_id]]
 
     process_payment
 
