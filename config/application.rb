e5e4047b1bbfe0e174076ce7fbbdb2e602f1e7e2
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-14 11:01:52 +0100

    Fill in bank account name

--- config/application.rb
+++ config/application.rb
@@ -151,6 +151,7 @@ module Registrations
     config.worldpay_moto_password = ENV['WCRS_WORLDPAY_MOTO_PASSWORD'] || 'PASSWORD'
     config.worldpay_moto_macsecret = ENV['WCRS_WORLDPAY_MOTO_MACSECRET'] || 'MACSECRET'
 
+    config.environment_agency_bank_account_name = 'Environment Agency'
     config.bank_transfer_account_number = '99999999'
     config.bank_transfer_sort_code = '99-99-99'
   end
