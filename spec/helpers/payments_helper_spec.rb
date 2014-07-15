7f43dac557909175ca7c0d0bb9105bf0502a6f1a
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-15 15:48:11 +0100

    Spec what this method does now but the answers are out because using pence

--- spec/helpers/payments_helper_spec.rb
+++ spec/helpers/payments_helper_spec.rb
@@ -5,4 +5,37 @@ describe PaymentsHelper do
     specify { helper.pence_to_currency(15400).should == '£154.00' }
     specify { helper.pence_to_currency(15900).should == '£159.00' }
   end
+
+  describe '#amountPaymentSummary_for' do
+    let(:registration) { Registration.new }
+    let(:finance_details) { double 'finance_details' }
+
+    before do
+      allow(registration).to receive(:financeDetails).and_return(finance_details)
+    end
+
+    context 'positive balance' do
+      before do
+        allow(finance_details).to receive(:balance).and_return(10000)
+      end
+
+      specify { helper.amountPaymentSummary_for(registration).should == 'Awaiting payment £10,000.00' }
+    end
+
+    context 'negative balance' do
+      before do
+        allow(finance_details).to receive(:balance).and_return(-10000)
+      end
+
+      specify { helper.amountPaymentSummary_for(registration).should == 'Overpaid by £10,000.00' }
+    end
+
+    context 'zero balance' do
+      before do
+        allow(finance_details).to receive(:balance).and_return(0)
+      end
+
+      specify { helper.amountPaymentSummary_for(registration).should == 'Paid in full' }
+    end
+  end
 end
