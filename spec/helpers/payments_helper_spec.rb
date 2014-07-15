47b8e5303907de905162a65824a8699dda823b12
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-15 16:25:09 +0100

    Specs passing

--- spec/helpers/payments_helper_spec.rb
+++ spec/helpers/payments_helper_spec.rb
@@ -38,4 +38,65 @@ describe PaymentsHelper do
       specify { helper.amountPaymentSummary_for(registration).should == 'Paid in full' }
     end
   end
+
+  describe '#amountSummary_for' do
+    let(:registration) { Registration.new }
+    let(:finance_details) { double 'finance_details' }
+
+    before do
+      allow(registration).to receive(:financeDetails).and_return(finance_details)
+    end
+
+    context 'include balance' do
+      context 'positive balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(10000)
+        end
+
+        specify { helper.amountSummary_for(registration, true).should == 'Awaiting payment £100.00' }
+      end
+
+      context 'negative balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(-10000)
+        end
+
+        specify { helper.amountSummary_for(registration, true).should == 'Overpaid by £100.00' }
+      end
+
+      context 'zero balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(0)
+        end
+
+        specify { helper.amountSummary_for(registration, true).should == 'Paid in full' }
+      end
+    end
+
+    context 'exclude balance' do
+      context 'positive balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(10000)
+        end
+
+        specify { helper.amountSummary_for(registration, false).should == 'Awaiting payment' }
+      end
+
+      context 'negative balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(-10000)
+        end
+
+        specify { helper.amountSummary_for(registration, false).should == 'Overpaid by' }
+      end
+
+      context 'zero balance' do
+        before do
+          allow(finance_details).to receive(:balance).and_return(0)
+        end
+
+        specify { helper.amountSummary_for(registration, false).should == 'Paid in full' }
+      end
+    end
+  end
 end
