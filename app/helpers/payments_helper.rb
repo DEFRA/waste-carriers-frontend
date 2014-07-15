c5807e81596fd19def9ad522ccba593828d4c084
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-15 16:46:28 +0100

    Remove some parens

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,20 +1,20 @@
 module PaymentsHelper
 
   # This is shown in the payment status view, the difference to amountSummary_for is it has awaiting payment text instead of balance due text
-  def amountPaymentSummary_for(model)
+  def amountPaymentSummary_for model
     amountSummary_for model, true
   end
 
   # This is shown in the enter payment view and payment status view
-  def amountSummary_for(model, includeBalance)
+  def amountSummary_for model, includeBalance
     balance = model.financeDetails.balance
 
     prefix = if balance > 0
-               t('registrations.form.awaitingPayment_text')
+               t 'registrations.form.awaitingPayment_text'
              elsif balance < 0
-               t('registrations.form.overpaid_text')
+               t 'registrations.form.overpaid_text'
              else
-               t('registrations.form.paidInFull_text')
+               t 'registrations.form.paidInFull_text'
              end
 
     suffix = " #{pence_to_currency balance.abs}" unless balance.zero?
