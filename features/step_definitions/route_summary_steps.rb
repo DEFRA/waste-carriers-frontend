25de45714be385761d6a7986b6b3f7e6d68420f3
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-17 12:17:05 +0100

    Autogenerate steps

---
+++ features/step_definitions/route_summary_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have come to the lower tier summary page$/) do
+  pending
+end
+
+Given(/^I have come to the upper tier summary page$/) do
+  pending
+end
+
+Then(/^I see I am a lower tier waste carrier$/) do
+  pending
+end
+
+Then(/^I see I am an upper tier waste carrier$/) do
+  pending
+end
