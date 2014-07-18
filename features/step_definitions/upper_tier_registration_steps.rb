e96bb05f844e6d9cf9939e20c0aad946e1bdb3a7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-07-18 16:20:52 +0100

    Added elec payment details to confirm email if balance not 0 and added test (plus some refactoring of existing tests) [#75187158].

--- features/step_definitions/upper_tier_registration_steps.rb
+++ features/step_definitions/upper_tier_registration_steps.rb
@@ -1,6 +1,15 @@
+Given(/^I have completed the upper tier and chosen to pay by bank transfer$/) do
+  step 'I have been funneled into the upper tier path'
+  step 'I am a carrier dealer'
+  step 'I enter my business details'
+  step 'I enter my contact details'
+  step 'I confirm the declaration'
+  step 'I enter new user account details'
+  step 'I pay via electronic transfer'
+end
+
 Given(/^I have been funneled into the upper tier path$/) do
   visit find_path
-
   choose 'registration_businessType_soletrader'
   click_on 'Next'
   choose 'registration_otherBusinesses_yes'
@@ -16,7 +25,39 @@ And(/^I am a carrier dealer$/) do
   click_on 'Next'
 end
 
-And(/^I go past the payment page$/) do
+And(/^I enter my business details$/) do
+  fill_in 'registration_companyName', with: 'Bespoke'
+  fill_in 'sPostcode', with: 'BS1 5AH'
+  click_on 'Find UK address'
+
+  select 'Environment Agency, Horizon House, Deanery Road, City Centre, Bristol BS1 5AH'
+  click_on 'Next'
+end
+
+And(/^I enter my contact details$/) do
+  fill_in 'First name', with: 'Joe'
+  fill_in 'Last name', with: 'Bloggs'
+  fill_in 'Job title', with: 'Chief Barber'
+  fill_in 'Phone number', with: '0117 926 9999'
+  fill_in 'Email address', with: my_email_address
+  click_on 'Next'
+end
+
+And(/^I confirm the declaration$/) do
+  check 'registration_declaration'
+  click_on 'Confirm'
+end
+
+Given(/^I enter new user account details$/) do
+  fill_in 'registration_accountEmail', with: my_email_address
+  fill_in 'registration_accountEmail_confirmation', with: my_email_address
+  fill_in 'registration_password', with: my_password
+  fill_in 'registration_password_confirmation', with: my_password
+
+  click_on 'Next'
+end
+
+And(/^I pay via electronic transfer$/) do
   click_on 'Pay via electronic transfer'
   click_on 'Next'
 end
@@ -25,47 +66,4 @@ Then(/^I am registered as an upper tier waste carrier$/) do
   page.should have_content 'is registered as an upper tier waste carrier'
   open_email my_email_address
   current_email.should have_content 'is registered as an upper tier waste carrier'
-end
-
-Given(/^I have gone through the upper tier waste carrier process$/) do
-  visit find_path
-
-  choose 'registration_businessType_soletrader'
-  click_on 'Next'
-  choose 'registration_otherBusinesses_yes'
-  click_on 'Next'
-  choose 'registration_isMainService_yes'
-  click_on 'Next'
-  choose 'registration_onlyAMF_no'
-  click_on 'Next'
-
-  choose 'registration_registrationType_carrier_dealer'
-  click_on 'Next'
-
-  fill_in 'registration_companyName', with: 'Bespoke'
-  fill_in 'sPostcode', with: 'BS1 5AH'
-  click_on 'Find UK address'
-
-  select 'Environment Agency, Horizon House, Deanery Road, City Centre, Bristol BS1 5AH'
-  click_on 'Next'
-
-  fill_in 'First name', with: 'Joe'
-  fill_in 'Last name', with: 'Bloggs'
-  fill_in 'Job title', with: 'Chief Barber'
-  fill_in 'Phone number', with: '0117 926 9999'
-  fill_in 'Email address', with: my_email_address
-  click_on 'Next'
-
-  check 'registration_declaration'
-  click_on 'Confirm'
-
-  fill_in 'registration_accountEmail', with: my_email_address
-  fill_in 'registration_accountEmail_confirmation', with: my_email_address
-  fill_in 'registration_password', with: my_password
-  fill_in 'registration_password_confirmation', with: my_password
-
-  click_on 'Next'
-
-  click_on 'Pay via electronic transfer'
-  click_on 'Next'
 end
