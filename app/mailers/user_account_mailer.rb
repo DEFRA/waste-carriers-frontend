ecc07ec3dd7b773f591e8ddf183d882175c8a1d1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-07-18 12:02:36 +0100

    Replaced default Devise mailer with custom mailer.

---
+++ app/mailers/user_account_mailer.rb
@@ -0,0 +1,7 @@
+class UserAccountMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+
+
+
+end
