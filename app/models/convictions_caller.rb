32aeb4fe6036c5100e1ec1acf757d996ff2d5fbc
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-24 15:57:11 +0100

    Extract constant

--- app/models/convictions_caller.rb
+++ app/models/convictions_caller.rb
@@ -1,13 +1,15 @@
 class ConvictionsCaller
   attr_reader :params
 
+  @@URL = 'http://localhost/convictions'
+
   def initialize options={}
     @params = options
   end
 
   def convicted?
     begin
-      json = JSON.parse RestClient.get 'http://localhost/convictions', params: @params
+      json = JSON.parse RestClient.get @@URL, params: @params
       json['isSuspect']
     rescue
       :error_calling_service
