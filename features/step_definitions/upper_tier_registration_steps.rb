3b45cb8824c10738314980435c1495e28a462af6
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-29 15:06:18 +0100

    Fix some cukes

--- features/step_definitions/upper_tier_registration_steps.rb
+++ features/step_definitions/upper_tier_registration_steps.rb
@@ -3,6 +3,7 @@ Given(/^I have completed the upper tier and chosen to pay by bank transfer$/) do
   step 'I am a carrier dealer'
   step 'I enter my business details'
   step 'I enter my contact details'
+  step 'no key people in the organisation have convictions'
   step 'I confirm the declaration'
   step 'I enter new user account details'
   step 'I choose pay via electronic transfer'
