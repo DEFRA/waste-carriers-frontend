92e5db9d0b2d0cb34e3326644aaac2df49ff0313
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-29 14:37:31 +0100

    Add step

--- features/step_definitions/upper_tier_registration_steps.rb
+++ features/step_definitions/upper_tier_registration_steps.rb
@@ -67,3 +67,7 @@ Then(/^I am registered as an upper tier waste carrier$/) do
   open_email my_email_address
   current_email.should have_content 'is registered as an upper tier waste carrier'
 end
+
+And(/^no key people in the organisation have convictions$/) do
+  pending
+end
