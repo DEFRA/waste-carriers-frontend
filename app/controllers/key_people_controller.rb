8fda66ec64c4ef6aca50264fb0de0307482c5f16
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-30 17:10:10 +0100

    Do some manual renaming

--- app/controllers/key_people_controller.rb
+++ app/controllers/key_people_controller.rb
@@ -2,14 +2,14 @@ require 'securerandom'
 
 class KeyPeopleController < ApplicationController
 
-  # GET /your-registration/directors
+  # GET /your-registration/key-people
   def registration
     get_registration
     unless @registration.businessType == "limitedCompany"
       redirect_to :upper_payment
       return
     end
-    get_directors
+    get_key_people
     unless @key_people.empty?
       redirect_to action: 'index'
       return
@@ -17,39 +17,36 @@ class KeyPeopleController < ApplicationController
     redirect_to action: 'new'
   end
 
-  # GET /your-registration/directors
+  # GET /your-registration/key-people
   def index
-    get_directors
+    get_key_people
   end
 
-  # GET /your-registration/directors/:id
+  # GET /your-registration/key-people/:id
   def show
   end
 
-  # GET /your-registration/directors/new
+  # GET /your-registration/key-people/new
   def new
-    # @director = Director.new
-    # @registration = Registration[ session[:registration_id]]
     @key_person = Registration::KeyPerson.create
   end
 
-  # GET /directors/edit/:id
+  # GET /key-people/edit/:id
   def edit
-    get_directors
+    get_key_people
     @key_person = Registration::KeyPerson[params[:id]]
     logger.debug "id to update: #{params[:id]}"
-    session[:director_update_id] = params[:id]
-    # @director = @directors.find { |d| d.temp_id == params[:id]}
+    session[:key_person_update_id] = params[:id]
   end
 
-  # GET /directors/delete/:id
+  # GET /key-people/delete/:id
   def delete
-    get_directors
-    director_to_remove = Registration::KeyPerson[params[:id]]
-logger.debug "reg is: #{@registration.id}"
-logger.debug "director_to_remove is: #{director_to_remove.id}"
+    get_key_people
+    key_person_to_remove = Registration::KeyPerson[params[:id]]
+    logger.debug "reg is: #{@registration.id}"
+    logger.debug "key person to remove is: #{key_person_to_remove.id}"
 
-    @registration.key_people.delete(director_to_remove)
+    @registration.key_people.delete(key_person_to_remove)
 
     if @registration.key_people.to_a.empty?
       redirect_to action: 'registration'
@@ -59,58 +56,56 @@ logger.debug "director_to_remove is: #{director_to_remove.id}"
 
   end
 
-  # POST /your-registration/directors
+  # POST /your-registration/key-people
   def create
-    get_directors
+    get_key_people
 
-    director = Registration::KeyPerson.create
-    director.set_attribs(params[:key_person])
+    key_person = Registration::KeyPerson.create
+    key_person.set_attribs(params[:key_person])
 
 
 
-    if director.valid?
-      director.save
+    if key_person.valid?
+      key_person.save
       logger.debug @registration.id
-      @registration.key_people.add(director )
+      @registration.key_people.add(key_person)
       @registration.save
 
-      logger.info 'Director is valid so far, go to next page'
+      logger.info 'Key person is valid so far, go to next page'
       render 'index'
     else
       # there is an error (but data not yet saved)
-      logger.info 'Director is not valid, and data is not yet saved'
+      logger.info 'Key person is not valid, and data is not yet saved'
       render :new, :status => '400'
     end
   end
 
-  # PATCH/PUT /your-registration/directors/:id
+  # PATCH/PUT /your-registration/key-person/:id
   def update
-    get_directors
+    get_key_people
 
-    # @director = Director.new(params[:director])
-
-    director = Registration::KeyPerson[ session[:director_update_id]]
-    director.set_attribs(params[:key_person])
+    key_person = Registration::KeyPerson[ session[:key_person_update_id]]
+    key_person.set_attribs(params[:key_person])
 
 
-    if director.valid?
-      director.save
-      logger.debug  director.attributes.to_s
-      logger.info 'Director is valid so far, go to next page'
+    if key_person.valid?
+      key_person.save
+      logger.debug  key_person.attributes.to_s
+      logger.info 'Key person is valid so far, go to next page'
       render 'index'
     else
       # there is an error (but data not yet saved)
-      logger.info 'Director is not valid, and data is not yet saved'
+      logger.info 'Key person is not valid, and data is not yet saved'
       render "edit", :status => '400'
     end
   end
 
-  # DELETE /directors/:id
+  # DELETE /key-person/:id
   def destroy
-    get_directors
+    get_key_person
   end
 
-  # POST /your-registration/directors/done
+  # POST /your-registration/key-people/done
   def done
     redirect_to :newRelevantConvictions
   end
@@ -121,22 +116,20 @@ logger.debug "director_to_remove is: #{director_to_remove.id}"
     logger.debug  @registration.attributes.to_s
   end
 
-  def get_directors
-    # session[:directors] ||= []
-    # @directors = session[:directors]
+  def get_key_people
     @registration = Registration[ session[:registration_id]]
     @key_people = @registration.key_people.to_a
     logger.debug  @key_people.to_s
   end
 
-  def set_directors
+  def set_key_people
     session[:key_people] ||= @key_people
   end
 
   private
 
   ## 'strong parameters' - whitelisting parameters allowed for mass assignment from UI web pages
-  def director_params
+  def key_person_params
     params.require(:key_person).permit(
       :first_name,
       :last_name,
