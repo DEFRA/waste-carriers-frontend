db7fc3319bc6c742c0b91e51ca03a9edcedd711c
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-31 15:14:32 +0100

    Specs are almost there but tricky because don't have callbacks

--- spec/models/key_person_spec.rb
+++ spec/models/key_person_spec.rb
@@ -7,4 +7,24 @@ describe KeyPerson do
   it { should validate_presence_of(:dob_day) }
   it { should validate_presence_of(:dob_month) }
   it { should validate_presence_of(:dob_year) }
+
+  describe 'dob' do
+    context 'past' do
+      Timecop.freeze('5 Jul 2014'.to_date) do
+        it { should allow_value('4 Jul 2014'.to_date).for(:dob) }
+      end
+    end
+
+    context 'today' do
+      Timecop.freeze('5 Jul 2014'.to_date) do
+        it { should_not allow_value('5 Jul 2014'.to_date).for(:dob) }
+      end
+    end
+
+    context 'future' do
+      Timecop.freeze('5 Jul 2014'.to_date) do
+        it { should_not allow_value('6 Jul 2014'.to_date).for(:dob) }
+      end
+    end
+  end
 end
