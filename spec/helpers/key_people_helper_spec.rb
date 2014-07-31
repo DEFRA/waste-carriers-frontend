3388a2fe3fb687678457fda6d425d1b82c59fa14
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-07-31 10:25:56 +0100

    Fix spec

--- spec/helpers/key_people_helper_spec.rb
+++ spec/helpers/key_people_helper_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe KeyPeopleHelper do
   describe '#first_name_last_name' do
-    specify { helper.first_name_last_name(Director.new(first_name: 'Barry', last_name: 'Butler') ).should == 'Barry Butler' }
-    specify { helper.first_name_last_name(Director.new(first_name: 'Ron Jon', last_name: 'Paul') ).should == 'Ron Jon Paul' }
+    specify { helper.first_name_last_name(KeyPerson.new(first_name: 'Barry', last_name: 'Butler') ).should == 'Barry Butler' }
+    specify { helper.first_name_last_name(KeyPerson.new(first_name: 'Ron Jon', last_name: 'Paul') ).should == 'Ron Jon Paul' }
   end
 end
