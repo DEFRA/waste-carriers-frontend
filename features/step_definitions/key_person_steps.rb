d81c8f7ea2675919ecb87dd0d74c867db8f3038a
Stinkypants <alan.cruikshanks@gmail.com>
2014-08-03 23:37:14 +0100

    Added tests for the new key person and key people functionality [#75960862].

--- features/step_definitions/key_person_steps.rb
+++ features/step_definitions/key_person_steps.rb
@@ -1,21 +1,54 @@
 And(/^I enter the details of the business owner$/) do
-
   fill_in 'key_person_first_name', with: 'Joe'
   fill_in 'key_person_last_name', with: 'Bloggs'
   fill_in 'key_person_dob_day', with: '01'
   fill_in 'key_person_dob_month', with: '02'
   fill_in 'key_person_dob_year', with: '1980'
-  click_on 'Next'
-
+  click_on 'next_btn'
 end
 
 And(/^I add a key person to the registration$/) do
-
   fill_in 'key_person_first_name', with: 'Joe'
   fill_in 'key_person_last_name', with: 'Bloggs'
   fill_in 'key_person_dob_day', with: '01'
   fill_in 'key_person_dob_month', with: '02'
   fill_in 'key_person_dob_year', with: '1980'
-  click_on 'add'
+  click_on 'add_key_person_btn'
+end
 
+And /^I add the following key people:$/ do |table|
+  table.hashes.each do |row|
+    fill_in 'key_person_first_name', with: row[:first_name]
+    fill_in 'key_person_last_name', with: row[:last_name]
+    fill_in 'key_person_dob_day', with: row[:dob_day]
+    fill_in 'key_person_dob_month', with: row[:dob_month]
+    fill_in 'key_person_dob_year', with: row[:dob_year]
+    click_on 'add_key_person_btn'
+  end
+end
+
+Then /^I should see the following names listed:$/ do |table|
+  table.hashes.each do |row|
+    page.should have_content row[:first_name]
+    page.should have_content row[:last_name]
+  end
+end
+
+And(/^I add a key person to the registration$/) do
+  fill_in 'key_person_first_name', with: 'Joe'
+  fill_in 'key_person_last_name', with: 'Bloggs'
+  fill_in 'key_person_dob_day', with: '01'
+  fill_in 'key_person_dob_month', with: '02'
+  fill_in 'key_person_dob_year', with: '1980'
+  click_on 'add_key_person_btn'
+end
+
+Then(/^I should only have to enter one key person$/) do
+  page.should_not have_button 'add_key_person_btn'
+  page.should have_button 'next_btn'
+end
+
+Then(/^I cannot proceed until I have added a key person$/) do
+  page.should_not have_button 'next_btn'
+  page.should have_button 'add_key_person_btn'
 end
--- features/step_definitions/upper_tier_registration_steps.rb
+++ features/step_definitions/upper_tier_registration_steps.rb
@@ -22,6 +22,18 @@ Given(/^I have been funneled into the upper tier path$/) do
   click_on 'Next'
 end
 
+Given(/^I am a partnership on the upper tier path$/) do
+  visit find_path
+  choose 'registration_businessType_partnership'
+  click_on 'Next'
+  choose 'registration_otherBusinesses_yes'
+  click_on 'Next'
+  choose 'registration_isMainService_yes'
+  click_on 'Next'
+  choose 'registration_onlyAMF_no'
+  click_on 'Next'
+end
+
 And(/^I am a carrier dealer$/) do
   choose 'registration_registrationType_carrier_dealer'
   click_on 'Next'
