e817d22fe76a7c8712701511bd18647fb02701c1
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-06 14:48:56 +0100

    First two scenarios passing

--- features/step_definitions/conviction_steps.rb.rb
+++ features/step_definitions/conviction_steps.rb.rb
@@ -6,15 +6,37 @@ Given(/^I fill out the upper tier steps$/) do
   step 'I enter the details of the business owner'
 end
 
-And(/^I do not declare any convictions$/) do
+And(/^I declare convictions$/) do
+  choose 'Yes'
+end
+
+And(/^I do not declare convictions$/) do
   choose 'No'
+end
+
+And(/^I enter convictee details$/) do
+  click_on 'Next'
+
+  fill_in 'First name', with: 'Barry'
+  fill_in 'Last name', with: 'Butler'
+  fill_in 'Position', with: 'Janitor'
+
+  fill_in 'Day', with: '1'
+  fill_in 'Month', with: '1'
+  fill_in 'Year', with: '1970'
+
+  click_on 'Add another person'
 end
 
 When(/^I come to the confirmation step$/) do
   click_on 'Next'
 end
 
-Then(/^I see my answer to the convictions question$/) do
+Then(/^I see I declared convictions$/) do
+  page.should have_content 'You have declared convictions'
+end
+
+Then(/^I see I did not declare convictions$/) do
   page.should have_content 'You have not declared any convictions'
 end
 
