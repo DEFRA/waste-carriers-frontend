a52607656185731327ff22ef96a0931b363dd27b
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-06 15:03:39 +0100

    Fill in steps but not passing yet

--- features/step_definitions/conviction_steps.rb.rb
+++ features/step_definitions/conviction_steps.rb.rb
@@ -50,3 +50,25 @@ And(/^this takes me back to the conviction step$/) do
   click_on 'Edit your conviction declaration'
   page.should have_content 'any relevant convictions in the last 12 months?'
 end
+
+But(/^the convictions service says I am suspect$/) do
+  allow_any_instance_of(ConvictionsCaller).to receive(:convicted?).and_return(true)
+end
+
+When(/^I come to the final step$/) do
+  check 'registration_declaration'
+  click_on 'Confirm'
+
+  fill_in 'registration_accountEmail_confirmation', with: my_email_address
+  fill_in 'registration_password', with: my_password
+  fill_in 'registration_password_confirmation', with: my_password
+  click_on 'Next'
+
+  click_on 'Pay via electronic transfer'
+
+  click_on 'Next'
+end
+
+Then(/^I am told my application is being checked$/) do
+  page.should have_content 'Your registration is being checked'
+end
