16ac2a26e003c0754242bfbb085c10acecf35f41
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-07 10:53:46 +0100

    Remove trailing forward slash

--- app/models/convictions_caller.rb
+++ app/models/convictions_caller.rb
@@ -1,7 +1,7 @@
 class ConvictionsCaller
   attr_reader :params
 
-  @@URL = 'http://localhost:9290/convictions/'
+  @@URL = 'http://localhost:9290/convictions'
 
   def initialize params={}
     params.assert_valid_keys :name, :dateOfBirth, :companyNumber
