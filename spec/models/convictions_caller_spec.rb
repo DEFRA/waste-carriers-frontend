fa63f669ae05cbd1d73202649315d5793ae6f310
Nigel Lowry <nigel-lowry@ultra.eclipse.co.uk>
2014-08-07 11:33:29 +0100

    Change text

--- spec/models/convictions_caller_spec.rb
+++ spec/models/convictions_caller_spec.rb
@@ -8,25 +8,25 @@ describe ConvictionsCaller do
 
   describe '.initialize' do
     context 'params for individual' do
-      it 'does not raise error' do
+      it 'does not error' do
         expect { ConvictionsCaller.new(name: 'Barry Butler', dateOfBirth: '1970-01-01') }.not_to raise_error
       end
     end
 
     context 'params for company' do
-      it 'does not raise error' do
+      it 'does not error' do
         expect { ConvictionsCaller.new(name: 'Acme Inc.', companyNumber: '99999999') }.not_to raise_error
       end
     end
 
     context 'mix of params for company and individual' do
-      it 'does not raise error' do
+      it 'does not error' do
         expect { ConvictionsCaller.new(name: 'Acme Inc.', companyNumber: '99999999', dateOfBirth: '1970-01-01') }.not_to raise_error
       end
     end
 
     context 'invalid params' do
-      it 'does raise error' do
+      it 'errors' do
         expect { ConvictionsCaller.new(name: 'Acme Inc.', companyNo: '99999999') }.to raise_error
       end
     end
