7b88e154300dca00028a2457409f8e2f696fd48f
steven-radford <stevenr@aptosolutions.co.uk>
2014-09-11 17:11:54 +0100

    Ensure Pending registrations can have no further user input

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -899,11 +899,11 @@ class Registration < Ohm::Model
   end
   
   def can_be_edited?
-    metaData.first.status != 'REVOKED' && metaData.first.status != 'EXPIRED'
+    metaData.first.status != 'REVOKED' && metaData.first.status != 'EXPIRED' && metaData.first.status != 'PENDING'
   end
 
   def can_view_certificate?
-    metaData.first.status != 'REVOKED' && metaData.first.status != 'EXPIRED'
+    metaData.first.status != 'REVOKED' && metaData.first.status != 'EXPIRED' && metaData.first.status != 'PENDING'
   end
 
   def can_request_copy_cards?
