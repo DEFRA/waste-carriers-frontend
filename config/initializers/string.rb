2e89100c92cc0cc491fbaa95053c91361a14241e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-09-15 12:20:33 +0100

    Updated comments related to fix [#77614536].

--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,6 +1,7 @@
 class String
 
   # Code below is attributed to http://stackoverflow.com/a/7085090
+  # though amended formats to match UK day-month standard
   def is_date?
     temp = self.gsub(/[-.\/]/, '')
     ['%d%m%Y','%d%m%y','%D%M%Y','%D%M%y'].each do |f|
