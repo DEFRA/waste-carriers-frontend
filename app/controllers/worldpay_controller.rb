c935b30e296f252b8d3489c9b885b9ad10c78844
Fred Heath <fred_h@bootstrap.me.uk>
2014-09-25 10:23:43 +0100

    eminem

--- app/controllers/worldpay_controller.rb
+++ app/controllers/worldpay_controller.rb
@@ -21,19 +21,19 @@ class WorldpayController < ApplicationController
         # new registrations
         next_step = if user_signed_in?
 
-            # Attempt to activate registration
-            Registration.send_registered_email(current_user, @registration)
+          # Attempt to activate registration
+          Registration.send_registered_email(current_user, @registration)
 
-            finish_path
-          elsif agency_user_signed_in?
+          finish_path
+        elsif agency_user_signed_in?
 
-            # Attempt to activate registration
-            Registration.send_registered_email(current_agency_user, @registration)
+          # Attempt to activate registration
+          Registration.send_registered_email(current_agency_user, @registration)
 
-            finishAssisted_path
-          else
-            confirmed_path
-          end
+          finishAssisted_path
+        else
+          confirmed_path
+        end
       when Order.edit_registration_identifier
         # edit/renew registration
         next_step = complete_edit_renew_path(@registration.uuid)
@@ -73,7 +73,7 @@ class WorldpayController < ApplicationController
       # Used to redirect_to WorldpayController::Error however that doesn't actually
       # exist, plus the plan as discussed with Georg was to redirect back to the payment
       # summary page but display the error details.
-      
+
       # Check if renderType and orderCode exist, If so its okay to redirect to order page, If not render Expired page
       if session[:renderType] and session[:orderCode]
         next_step = upper_payment_path(@registration.uuid)
@@ -83,12 +83,33 @@ class WorldpayController < ApplicationController
       end
     end
 
+    if @registration.commit #create new reg
+      logger.debug "payment new reg id #{@registration.id}"
+      original_reg = Registration[ session[:original_registration_id] ]
+      logger.debug "original reg id #{original_reg.id}"
+      @registration.set_inactive #deactivate existing reg
+    else
+      logger.error "Commit failed for registration id: #{@registration.id}"
+      #TODO: error handlling
+    end
+
+ logger.debug next_step.to_s
+ logger.debug next_step.class.to_s
+
+    if next_step == complete_edit_renew_path(@registration.uuid).to_s
+       logger.debug "setting next step"
+      next_step = complete_edit_renew_path(id: @registration.uuid, edit_mode: session[:edit_mode], 
+                  edit_result: session[:edit_result])
+      logger.debug next_step.to_s
+      clear_edit_session # we don't need edit variables polluting the session any more
+    end
+
     redirect_to next_step
 
   end
 
   def failure
-  	@registration = Registration.find_by_id(session[:registration_uuid])
+    @registration = Registration.find_by_id(session[:registration_uuid])
     if process_payment
       # Should not get here as payment should have failed and thus return false
     else
@@ -98,12 +119,12 @@ class WorldpayController < ApplicationController
   end
 
   def pending
-  	#TODO - Process response and edirect...
+    #TODO - Process response and edirect...
     process_payment
   end
 
   def cancel
-  	@registration = Registration.find_by_id(session[:registration_uuid])
+    @registration = Registration.find_by_id(session[:registration_uuid])
     if process_payment
       # should not get here as payment was cancelled
     else
@@ -144,131 +165,136 @@ class WorldpayController < ApplicationController
 
   private
 
-    def process_payment
+  def process_payment
+    payment_processed = false
+    orderKey = params[:orderKey] || ''
+    paymentAmount = params[:paymentAmount] || ''
+    paymentCurrency = params[:paymentCurrency] || ''
+    paymentStatus = params[:paymentStatus] || ''
+    mac = params[:mac] || ''
+    if !validate_worldpay_return_parameters(orderKey,paymentAmount,paymentCurrency,paymentStatus,mac)
+      logger.error 'Validation of Worldpay return parameters failed. MAC verification failed!'
+      # TODO Possibly need to do something more meaningful with the fact the MAC check has failed
+
+      # Update order to reflect failed payment status
+      orderCode = orderKey.split('^').at(2)
+      order = @registration.getOrder( orderCode)
+      now = Time.now.utc.xmlschema
+      order.dateLastUpdated = now
+      order.worldPayStatus = 'VERIFICATIONFAILED'
+      order.save! session[:registration_uuid]
+
       payment_processed = false
-      orderKey = params[:orderKey] || ''
-      paymentAmount = params[:paymentAmount] || ''
-      paymentCurrency = params[:paymentCurrency] || ''
-      paymentStatus = params[:paymentStatus] || ''
-      mac = params[:mac] || ''
-      if !validate_worldpay_return_parameters(orderKey,paymentAmount,paymentCurrency,paymentStatus,mac)
-        logger.error 'Validation of Worldpay return parameters failed. MAC verification failed!'
-        # TODO Possibly need to do something more meaningful with the fact the MAC check has failed
+    elsif paymentStatus.eql? 'AUTHORISED'
+      orderCode = orderKey.split('^').at(2)
 
-        # Update order to reflect failed payment status
-        orderCode = orderKey.split('^').at(2)
-        order = @registration.getOrder( orderCode)
-        now = Time.now.utc.xmlschema
-        order.dateLastUpdated = now
-        order.worldPayStatus = 'VERIFICATIONFAILED'
-        order.save! session[:registration_uuid]
+      now = Time.now.utc
+      #now = Time.now.utc.xmlschema
+      @payment = Payment.new
+      @payment.dateReceived = now
+      @payment.dateReceived_year = now.year
+      @payment.dateReceived_month = now.month
+      @payment.dateReceived_day = now.day
+      #We don't need to set the dateEntered; this is done within the service
+      #@payment.dateEntered = now
+      # TODO get the user if not yet logged in (still to be activated)
+      @payment.updatedByUser = @registration.accountEmail
+      @payment.amount = paymentAmount.to_i
+      @payment.orderKey = orderCode
+      @payment.currency = paymentCurrency
+      @payment.paymentType = 'WORLDPAY'
+      @payment.worldPayPaymentStatus = paymentStatus
+      @payment.mac_code = mac
+      @payment.registrationReference = 'Worldpay'
+      @payment.comment = 'Paid via Worldpay'
 
-        payment_processed = false
-      elsif paymentStatus.eql? 'AUTHORISED'
-        orderCode = orderKey.split('^').at(2)
-
-        now = Time.now.utc
-        #now = Time.now.utc.xmlschema
-        @payment = Payment.new
-        @payment.dateReceived = now
-        @payment.dateReceived_year = now.year
-        @payment.dateReceived_month = now.month
-        @payment.dateReceived_day = now.day
-        #We don't need to set the dateEntered; this is done within the service
-        #@payment.dateEntered = now
-        # TODO get the user if not yet logged in (still to be activated)
-        @payment.updatedByUser = @registration.accountEmail
-        @payment.amount = paymentAmount.to_i
-        @payment.orderKey = orderCode
-        @payment.currency = paymentCurrency
-        @payment.paymentType = 'WORLDPAY'
-        @payment.worldPayPaymentStatus = paymentStatus
-        @payment.mac_code = mac
-        @payment.registrationReference = 'Worldpay'
-        @payment.comment = 'Paid via Worldpay'
-
-        #TODO re-enable validation and saving - current validation rules are geared towards offline payments
-        if @payment.valid?
-          logger.debug "registration uuid: #{session[:registration_uuid]}"
-          if @payment.save! session[:registration_uuid]
-            #@payment.save(:validate => false)
-            payment_processed = true
-          end
-        else
-          logger.error 'Payment is not valid! ' + @payment.errors.messages.to_s
-          payment_processed = false
-          #TODO: what does this do? -need to replace it with explicit save
-          @payment.save(:validate => false)
+      #TODO re-enable validation and saving - current validation rules are geared towards offline payments
+      if @payment.valid?
+        logger.debug "registration uuid: #{session[:registration_uuid]}"
+        if @payment.save! session[:registration_uuid]
+          #@payment.save(:validate => false)
+          payment_processed = true
         end
       else
-        orderCode = orderKey.split('^').at(2)
-        logger.error 'Payment status was not successful, paymentStatus: ' + paymentStatus.to_s + " for order: " + orderCode.to_s
-
-        # Update order to reflect failed payment status
-        order = @registration.getOrder( orderCode)
-        now = Time.now.utc.xmlschema
-        order.dateLastUpdated = now
-        order.worldPayStatus = paymentStatus
-        order.save! session[:registration_uuid]
-
+        logger.error 'Payment is not valid! ' + @payment.errors.messages.to_s
         payment_processed = false
+        #TODO: what does this do? -need to replace it with explicit save
+        @payment.save(:validate => false)
       end
+    else
+      orderCode = orderKey.split('^').at(2)
+      logger.error 'Payment status was not successful, paymentStatus: ' + paymentStatus.to_s + " for order: " + orderCode.to_s
 
-      payment_processed
+      # Update order to reflect failed payment status
+      order = @registration.getOrder( orderCode)
+      now = Time.now.utc.xmlschema
+      order.dateLastUpdated = now
+      order.worldPayStatus = paymentStatus
+      order.save! session[:registration_uuid]
+
+      payment_processed = false
     end
 
-    def update_order
-      #TODO better pass in as variables?
-      orderCode = @payment.orderKey
-      status = @payment.worldPayPaymentStatus
+    payment_processed
+  end
 
-      reg = Registration.find_by_id(session[:registration_uuid])
+  def update_order
+    #TODO better pass in as variables?
+    orderCode = @payment.orderKey
+    status = @payment.worldPayPaymentStatus
 
-      # Get the current_order from the registration
-      ord = reg.getOrder( orderCode)
-      #ord = reg.finance_details.first.orders.first
+    reg = Registration.find_by_id(session[:registration_uuid])
 
-      @order = Order.init(ord.attributes)
+    # Get the current_order from the registration
+    ord = reg.getOrder( orderCode)
+    #ord = reg.finance_details.first.orders.first
 
-      #TODO Will need to set other payment methods accordingly
-      now = Time.now.utc.xmlschema
-      #@order.id = ord.id
-      #@order.merchantId = worldpay_merchant_code
-      #@order.totalAmount = reg.total_fee
-      #@order.orderCode = orderCode
-      @order.worldPayStatus = status
-      @order.dateLastUpdated = now
-      @order.updatedByUser = reg.accountEmail
-     #@order.description = 'Updated order in WP contorller'
-     #@order.prefix_options[:id] = session[:registration_id]
+    @order = Order.init(ord.attributes)
 
-      @order.save
+    #TODO Will need to set other payment methods accordingly
+    now = Time.now.utc.xmlschema
+    #@order.id = ord.id
+    #@order.merchantId = worldpay_merchant_code
+    #@order.totalAmount = reg.total_fee
+    #@order.orderCode = orderCode
+    @order.worldPayStatus = status
+    @order.dateLastUpdated = now
+    @order.updatedByUser = reg.accountEmail
+    #@order.description = 'Updated order in WP contorller'
+    #@order.prefix_options[:id] = session[:registration_id]
 
-      logger.info '---- Adding to list: '
-      ord.order_items.each do |item|
-        logger.info 'item: ' + item.to_json.to_s
-        # Test Re-add order items from original order
-        @order.order_items.add item
-      end
+    @order.save
 
-      logger.info  '***** The @order is:'
-      logger.info  @order.attributes
-      logger.info  '*****'
+    logger.info '---- Adding to list: '
+    ord.order_items.each do |item|
+      logger.info 'item: ' + item.to_json.to_s
+      # Test Re-add order items from original order
+      @order.order_items.add item
+    end
 
-      if @order.valid?
-        if @order.save! reg.uuid
-          # Re-get registration so its data is up to date, for later use
-          @registration = Registration.find_by_id(session[:registration_uuid])
-          logger.info 'Re-populated @registration from the database'
-        else
-          # Errored saving order
-          logger.error "CODE ERROR: this should not happen if code implemented propery"
+    logger.info  '***** The @order is:'
+    logger.info  @order.attributes
+    logger.info  '*****'
+
+    if @order.valid?
+      if @order.save! reg.uuid
+        if session[:edit_mode]
+          logger.debug "edited registration id: #{@registration.id}"
+          logger.error "Failed to save registration #{@registration.id}" unless @registration.save!
         end
+        # Re-get registration so its data is up to date, for later use
+        @registration = Registration.find_by_id(session[:registration_uuid])
+        logger.debug "#{@registration.id}"
+        logger.info 'Re-populated @registration from the database'
       else
+        # Errored saving order
         logger.error "CODE ERROR: this should not happen if code implemented propery"
-        logger.error "The order is not valid! " + @order.errors.full_messages.to_s
       end
+    else
+      logger.error "CODE ERROR: this should not happen if code implemented propery"
+      logger.error "The order is not valid! " + @order.errors.full_messages.to_s
+    end
 
-    end #update_order
+  end #update_order
 
 end
