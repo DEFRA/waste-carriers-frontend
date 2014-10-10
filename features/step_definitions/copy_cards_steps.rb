4d7978cbb7459c5bcfa13ae50fab6c197da6ae0f
steven-radford <stevenr@aptosolutions.co.uk>
2014-10-10 14:19:39 +0100

    [#78254650] Update tests for checking order totals

--- features/step_definitions/copy_cards_steps.rb
+++ features/step_definitions/copy_cards_steps.rb
@@ -17,14 +17,41 @@ Given(/^I'm on the copy cards payment summary page$/) do
   page.should have_content 'Copy cards'
 end
 
-Given(/^Total charge will be (\d+)\.(\d+)$/) do |arg1, arg2|
-  totalParams = arg1 + '.' + arg2
-  # both the total fee and copy card fee should be the same
-  totalValue = find_by_id('registration_copy_card_fee').value.to_s
-  #totalValue = find_by_id('registration_total_fee').value.to_s
-  totalValue.should equal totalParams
+# Removed as replaced with a check on Worldpay's page rather than reply on JS on the current page
+#Given(/^Total charge will be (\d+)\.(\d+)$/) do |arg1, arg2|
+#  totalParams = arg1 + '.' + arg2
+#  # both the total fee and copy card fee should be the same
+#  totalValue = find_by_id('registration_copy_card_fee').value.to_s
+#  #totalValue = find_by_id('registration_total_fee').value.to_s
+#  totalValue.should equal totalParams
+#end
+
+Then(/^I will be shown confirmation of paid order$/) do  
+  # Payment Checks
+  page.should have_content 'Your order has been sucessful'
+  # Content specific checks
+  page.should have_content 'Thank you for your copy card order'
+  
+  click_on 'Finish'
+  # Check routing after clicking finish
+  page.should have_content 'Your registrations'  
+  
+  #pending # express the regexp above with the code you wish you had
 end
 
-Then(/^I will be shown confirmation of order$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^I will be shown confirmation of unpaid order$/) do  
+  # Payment Checks
+  page.should have_content 'Almost there'
+  # Content specific checks
+  page.should have_content 'Thank you for your copy card order'
+  
+  click_on 'Finish'
+  # Check routing after clicking finish
+  page.should have_content 'Your registrations'  
+  
+end
+
+Then(/^I finish my registration$/) do
+  # Perform this as it cleans the session variables 
+  click_on 'Finish'
 end
