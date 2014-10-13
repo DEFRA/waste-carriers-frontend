31d46f42c21f031f76d42db79aa9274f9efe010a
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-10-13 19:23:55 +0100

    Not allowing waste carrier login via internal admin URLs (PT 80471928)

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -29,3 +29,46 @@ Then(/^the user should see a login error$/) do
   page.should have_content 'Invalid email or password.'
 end
 
+
+When(/^the user tries to access the internal admin login URL from the public domain$/) do
+  base_url = 'http://' + Rails.configuration.waste_exemplar_frontend_url
+  url = base_url + new_user_session_path
+  visit url
+end
+
+Then(/^the page is not found$/) do
+  page.should_not have_content "Sign in"
+end
+
+When(/^the user tries to access the internal agency login URL from the public domain$/) do
+  base_url = 'http://' + Rails.configuration.waste_exemplar_frontend_url
+  url = base_url + new_agency_user_session_path
+  visit url
+end
+
+When(/^the user tries to access the internal admin login URL from the admin domain$/) do
+  base_url = 'http://' + Rails.configuration.waste_exemplar_frontend_admin_url
+  url = base_url + new_admin_session_path
+  visit url
+end
+
+Then(/^the admin login page is shown$/) do
+  page.should have_content "Sign in"
+end
+
+When(/^the user tries to access the internal agency login URL from the admin domain$/) do
+  base_url = 'http://' + Rails.configuration.waste_exemplar_frontend_admin_url
+  url = base_url + new_agency_user_session_path
+  visit url
+end
+
+Then(/^the agency user login page is shown$/) do
+  page.should have_content "Sign in"
+end
+
+When(/^the user tries to access the user login URL from the internal admin domain$/) do
+  base_url = 'http://' + Rails.configuration.waste_exemplar_frontend_admin_url
+  url = base_url + new_user_session_path
+  visit url
+end
+
