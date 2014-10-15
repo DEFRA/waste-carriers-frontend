c9e0029cdce5d00a5ab549ea3f28770a14c5a75f
steven-radford <stevenr@aptosolutions.co.uk>
2014-10-15 10:40:24 +0100

    Add in alternative debugging if save_and_open fails

--- features/support/debugging.rb
+++ features/support/debugging.rb
@@ -1,3 +1,12 @@
 After do |scenario|
-  save_and_open_page if scenario.failed?
+  if scenario.failed?
+    if save_and_open_page
+      # perform no additional actions
+    else
+      puts '' if scenario.failed?
+      puts 'EXCEPTION:: URL of the page with the test failure: '+page.current_path.to_s if scenario.failed?
+      puts '' if scenario.failed?
+      print page.html
+    end
+  end
 end
