ce9e90840766c44a84b2f2231d84e74b866e2b9c
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-11-19 17:45:30 +0000

    Removing sauce from tests

--- spec/features/registration_flow_spec.rb
+++ spec/features/registration_flow_spec.rb
@@ -1,7 +1,8 @@
 
 require "spec_helper"
 
-describe "The Registration flow", :sauce => true do
+# Re-enable Sauce with :sauce => true
+describe "The Registration flow" do
   it "The user should be able to go through the registration flow" do
     visit "/registrations/find"
     page.should have_content "Find out if you need to register and which category applies"
