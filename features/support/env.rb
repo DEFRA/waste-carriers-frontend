8e555a3a0147e92e92fa6db7c2510878700f6d40
Georg Mueller-Koegler <gmueller-koegler@caci.co.uk>
2014-11-19 17:53:01 +0000

    Removing sauce from tests

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@
 require 'cucumber/rails'
 
 # Added for cross-browser testing using Saucelabs
-require 'sauce/cucumber'
+# require 'sauce/cucumber'
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
