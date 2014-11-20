9cfe7d1982071da0e6a340974e5e486069d5cc99
steven-radford <stevenr@aptosolutions.co.uk>
2014-11-20 12:34:26 +0000

    Update language definition for the latest version of rails
    to enable switching of languages at a later date

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -3,6 +3,8 @@ I18n.enforce_available_locales = true
 
 I18n.default_locale = :en
 
-LANGUAGES = [
-  ['English','en']
-]
+I18n.available_locales = [:en]
+
+# Add an additional parameter to the array to include additional langauges, for example
+#I18n.available_locales = [:en, :cy]
+
