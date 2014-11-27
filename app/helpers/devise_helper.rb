8163c8369aa60ae0d2f0d24176e0092924f6e19a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2014-11-27 15:20:08 +0000

    Added ability to use translations for error messages related to invalid confirmation tokens [#83390032]

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -16,4 +16,23 @@ module DeviseHelper
 
     html.html_safe
   end
+
+  def devise_token_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.map { |err| content_tag(:li, I18n.t("errors.messages.#{err}")) }.join
+    sentence = I18n.t("errors.messages.not_saved",
+    :count => resource.errors.count,
+    :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div id="error_explanation" class="validation-summary">
+      <h2>#{sentence}</h2>
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
 end
