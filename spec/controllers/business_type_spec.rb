d47a7c34a23beaf584eb89034d8ae9d33ae761a0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-01-11 23:26:17 +0000

    Added second working test whilst also working on third.

--- spec/controllers/business_type_spec.rb
+++ spec/controllers/business_type_spec.rb
@@ -2,13 +2,38 @@ require 'spec_helper'
 
 describe BusinessTypeController, :type => :controller do
 
-  describe "GET #show" do
+  describe 'GET #show' do
 
-    it 'renders the show template' do
+    it 'responds successfully with an HTTP 200 status code' do
+      get :show
+      expect(response.code).to eq('200')
+    end
+
+    it 'renders the #show template' do
         get :show
         expect(response).to render_template("show")
     end
 
   end
+
+  # describe 'POST #create' do
+  #   context "when 'sole trader' is selected" do
+  #     # it 'sets #businessType on the registration' do
+  #     # end
+  #     it "redirects to the 'other businesses' page" do
+  #       post :create, registration: Registration.ctor
+  #     end
+  #   end
+  #
+  #
+  #   # context 'with valid attributes' do
+  #   #
+  #   #
+  #   # end
+  #   #
+  #   # context 'with invalid attributes' do
+  #   #
+  #   # end
+  # end
 
 end
