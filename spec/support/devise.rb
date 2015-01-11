fc9662cfb7a71689a120ac4896478a5092a7103a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-01-11 23:26:17 +0000

    Added second working test whilst also working on third.

--- spec/support/devise.rb
+++ spec/support/devise.rb
@@ -1,3 +1,9 @@
+# This file is required in projects that use devise but also wish to test controllers.
+# We identified this when I encountered this issue:
+# http://stackoverflow.com/questions/4308094/all-ruby-tests-raising-undefined-method-authenticate-for-nilnilclass
+# Adding this file and what it should contain is covered here:
+# https://github.com/plataformatec/devise#test-helpers
+
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 end
