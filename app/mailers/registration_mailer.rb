1016542dbc41306b6b468a73cec573d6a542a64d
Tom Thurston <tomthurstonwork@live.com>
2015-01-12 14:05:45 +0000

    [#83752836] and [#82547794]: remove obsolete material used to generate the revocation email.

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,6 +1,6 @@
 class RegistrationMailer < ActionMailer::Base
   default from: "registrations@wastecarriersregistration.service.gov.uk"
-  
+
   # This line should have set the defailt from name, but didnt in testing,
   # as such email_with_name variables were created to pass in the email name
   #default :from => "\"EA Waste Carriers\" <registrations@wastecarriersregistration.service.gov.uk>"
@@ -14,17 +14,7 @@ class RegistrationMailer < ActionMailer::Base
     	from: email_with_name,
     	reply_to: Rails.configuration.registrations_service_email)
   end
-  
-  def revoke_email(user, registration)
-  	@user = user
-  	@url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
-  	@registration = registration
-  	email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    mail(to: @user.email, subject: I18n.t('global.mailer.revoke.subject',:tier=>@registration.tier.downcase, :companyName=>@registration.companyName),
-    	from: email_with_name,
-    	reply_to: Rails.configuration.registrations_service_email)
-  end
-  
+
   # Call via: RegistrationMailer.awaitingPayment_email(@user, @registration).deliver
   def awaitingPayment_email(user, registration)
   	@user = user
@@ -36,7 +26,7 @@ class RegistrationMailer < ActionMailer::Base
     	from: email_with_name,
     	reply_to: Rails.configuration.registrations_service_email)
   end
-  
+
   # Call via: RegistrationMailer.awaitingConvictionsCheck_email(@user, @registration).deliver
   def awaitingConvictionsCheck_email(user, registration)
   	@user = user
@@ -48,8 +38,8 @@ class RegistrationMailer < ActionMailer::Base
     	from: email_with_name,
     	reply_to: Rails.configuration.registrations_service_email)
   end
-  
-  
+
+
   ## TODO - Replace with helper or decorator
   def format_date(string)
     d = string.to_date
