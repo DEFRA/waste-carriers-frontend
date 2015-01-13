240b0e806d331f8f7f64a8157ca76ec30d8d4e77
Tom Thurston <tomthurstonwork@live.com>
2015-01-13 16:32:30 +0000

    [#86026888] Ensure all databases (Mongo, Redis, ElasticSearch) are cleaned between tests.  Also some tweaks to improve speed of the Cucumber tests.

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -9,20 +9,28 @@ rescue NameError
 end
 
 Before do
-#  DatabaseCleaner.start
+  # Setup cleaning for the Mongo DB.
+  DatabaseCleaner.start
+
+  # Clean the Redis DB ([un]comment lines as required to prove code has desired effect).
+  Ohm.redis.call "FLUSHDB"   # Cleans the Redis DB.
+  #puts "Redis DB currently has #{Ohm.redis.call "DBSIZE"} keys"
+
+  # Clean the Elastic Search database if in development environment.
+  if !Rails.env.production?
+    #sleep 0.25    # Doesn't seem to be necessary.
+
+    # Old version: goes via the WCR Services layer, which deletes each registration one-by-one.
+    # Manually call the services to clear down any existing regitrations
+    #RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/dbcleaner', :content_type => :json, :accept => :json
+
+    # New version: direct call into ElasticSearch REST API.
+    RestClient.delete "#{Rails.configuration.waste_exemplar_elasticsearch_url}/registrations/_query?q=*:*"
+
+    #sleep 0.5       # Doesn't seem to be necessary.
+  end
 end
 
 After do |scenario|
   DatabaseCleaner.clean
-
-  # Checks if in development mode
-  if !Rails.env.production?
-
-    sleep 0.25
-
-    # Manually call the services to clear down any existing regitrations
-    RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/dbcleaner', :content_type => :json, :accept => :json
-
-    sleep 0.5
-  end
 end
