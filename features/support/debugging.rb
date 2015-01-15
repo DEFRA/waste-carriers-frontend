e73091afd0086fca833ff675fc71d4a8a4ccced5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-01-15 16:36:39 +0000

    We expect launchy to fail in jenkins and therefore don't need the error message in the output.

--- features/support/debugging.rb
+++ features/support/debugging.rb
@@ -5,7 +5,6 @@ After do |scenario|
     rescue Exception=>e
       # handle e
       puts ''
-      puts 'Error trying to open browser: ' + e.to_s
       puts 'EXCEPTION:: URL of the page with the test failure: '+page.current_path.to_s
       puts ''
       puts page.html
@@ -13,8 +12,6 @@ After do |scenario|
         begin
           current_email.save_and_open
         rescue Exception=>e
-          puts ''
-          puts 'Could not open email in browser, printing to console: ' + e.to_s
           puts ''
           puts current_email.body
         end
