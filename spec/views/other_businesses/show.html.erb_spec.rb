c4c193258570ddc82289a2b49aa6c3b87a3be89f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-01-17 23:49:58 +0000

    Added view spec for other businesses

---
+++ spec/views/other_businesses/show.html.erb_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'other_businesses/show', :type => :view do
+
+  it 'has autocomplete turned off in the form' do
+    assign(:registration, Registration.ctor(attrs={:id => 1}))
+    render
+    expect(rendered).to have_selector("form [autocomplete='off']")
+  end
+
+  context "when first visting the page" do
+
+    it "has no option pre-selected" do
+      assign(:registration, Registration.ctor(attrs={:id => 1}))
+      render
+      expect(rendered).to_not have_selector('[checked=checked]')
+    end
+
+  end
+
+  context "when having previously selected 'Yes'" do
+
+    it "has the matching option selected" do
+      assign(:registration, Registration.ctor(attrs={:otherBusinesses => 'yes'}))
+      render
+      expect(rendered).to have_selector('[id=registration_otherBusinesses_yes][checked=checked]')
+    end
+  end
+
+  context "when having previously selected 'No'" do
+
+    it "has the matching option selected" do
+      assign(:registration, Registration.ctor(attrs={:otherBusinesses => 'no'}))
+      render
+      expect(rendered).to have_selector('[id=registration_otherBusinesses_no][checked=checked]')
+    end
+  end
+
+end
