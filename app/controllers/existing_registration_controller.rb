c8fd49b240e77962a842a4277a390cabff6ed33e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-01-18 23:10:01 +0000

    Refactored what-type-of-registration-are-you into existing_registration controller and view

---
+++ app/controllers/existing_registration_controller.rb
@@ -0,0 +1,78 @@
+class ExistingRegistrationController < ApplicationController
+  include RegistrationsHelper
+
+  # GET /your-registration/existing-registration
+  def show
+    new_step_action 'enterRegNumber'
+  end
+
+  # POST /your-registration/existing-registration
+  def create
+    setup_registration 'enterRegNumber'
+    return unless @registration
+
+    # Validate which type of registration applied with, legacy IR system, Lower, or Upper current system
+    if @registration.originalRegistrationNumber.blank?
+      # If orignalRegistrationNumber not found, error
+      @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.blank'))
+    else
+
+      # Check current format
+      if isCurrentRegistrationType @registration.originalRegistrationNumber
+        # regNo matched
+
+        # redirect to sign in page
+        logger.debug "Current registration matched, Redirect to user sign in"
+        redirect_to :new_user_session
+        return
+        # Check old format
+      elsif isIRRegistrationType @registration.originalRegistrationNumber
+        # legacy regNo matched
+
+        # Call IR services to import IR registraion data
+        irReg = Registration.find_by_ir_number(@registration.originalRegistrationNumber)
+        if irReg
+          # IR data found, merge with registration
+
+          # Save IR registration data to session, for comparison at payment time
+          session[:original_registration_id] = irReg.id
+
+          # Access Code is one of the registration variables that should not get overriden with IR data
+          # so it is saved and reapplied after the merge
+          accessCode = @registration.accessCode
+
+          # Merge params registration with registration in memory
+          @registration.add( irReg.attributes )
+
+          # re-apply accessCode
+          @registration.accessCode = accessCode
+
+          @registration.save
+
+          logger.debug "Legacy registration matched, Redirect to smart answers"
+          redirect_to :business_type
+          return
+        else
+          # No IR data found
+          @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.ir_notFound'))
+        end
+        # Error not matched
+      else
+        @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.invalid'))
+      end
+    end
+
+    # Error must have occured, re-render view
+    render 'show', :status => '400'
+
+  end
+
+  private
+
+  ## 'strong parameters' - whitelisting parameters allowed for mass assignment from UI web pages
+  def registration_params
+    params.require(:registration).permit(
+    :originalRegistrationNumber)
+  end
+
+end
