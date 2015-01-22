11f067f53b3e4470574d9660b624bd59b73c21da
mj2p <mj2p.sg@gmail.com>
2015-01-22 14:56:24 +0000

    added irrenewals model

---
+++ app/models/irrenewals.rb
@@ -0,0 +1,15 @@
+class Irrenewals
+
+  include Mongoid::Document
+
+  field :applicantType,         :type => String,  :default => ''
+  field :expiryDate,            :type => Date,    :default => '0000-01-01T00:00:00.000Z'
+  field :referenceNumber,       :type => String,  :default => ''
+  field :registrationType,      :type => String,  :default => ''
+  field :irType,                :type => String,  :default => ''
+  field :companyName,           :type => String,  :default => ''
+  field :tradingName,           :type => String,  :default => ''
+  field :companyNumber,         :type => String,  :default => ''
+  field :trueRegistrationType,  :type => String,  :default => ''
+
+end
