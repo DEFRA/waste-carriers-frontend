2408e79d6f9863fdaf83ef3d899ea8d0f3ffa57f
Tom Thurston <tomthurstonwork@live.com>
2015-02-20 15:36:34 +0000

    Tidy up file to adhere to Ruby Style Guide, as part of previous edits.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -15,31 +15,29 @@ class ConfirmationsController < Devise::ConfirmationsController
   private
 
   def after_confirmation_path_for(resource_name, resource)
-
     Registration.activate_registrations(resource)
 
-  	#Temporarily storing the confirmed user in the session to be picked up from the registrations controller
+    # Temporarily storing the confirmed user in the session to be picked up from
+    # the registrations controller
     session[:user] = resource
 
-    # This is a variable that represents the page to be redirected to after the verification link
+    # This is a variable that represents the page to be redirected to after the
+    # verification link
     registrations = Registration.find_by_email(@user.email)
-    unless registrations.empty?
-    
+    if registrations.empty?
+      flash[:notice] = 'Registration list is empty, Found no registrations for user: ' + @user.email.to_s
+      ## todo put here path to sign in page
+      goto_page = new_user_session_path
+    else
       sorted = registrations.sort_by { |r| r.date_registered }.reverse!
       registration = sorted.first
       session[:registration_uuid] = registration.uuid
-      
       if registration.tier.eql? 'LOWER'
         goto_page = confirmed_path
       else
         goto_page = account_confirmed_path
       end
-    else
-      flash[:notice] = 'Registration list is empty, Found no registrations for user: ' + @user.email.to_s
-      ## todo put here path to sign in page
-      goto_page = new_user_session_path
     end
     goto_page
   end
-
 end
