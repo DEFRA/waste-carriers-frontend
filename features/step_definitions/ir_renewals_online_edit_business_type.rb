6dcf614ee7b285e2b40b66b663bd08b5046b43ab
Tim Stone <tim.stone@environment-agency.gov.uk>
2015-02-23 09:27:22 +0000

    pages update and IR AD test steps updated

--- features/step_definitions/ir_renewals_online_edit_business_type.rb
+++
@@ -1,9 +0,0 @@
-  
-Given(/^I change business type to Sole Trader$/) do
-  choose 'registration_businessType_soletrader'
-  click_button 'continue'
-end
-
- Then(/^I should be told that I have to start a new registration$/) do
-  page.should have_content 'new upper tier registration will be made and will require a payment of £154.00'
-end
