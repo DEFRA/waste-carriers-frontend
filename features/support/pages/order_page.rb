cfd6e63529cf64893ed16a4dda53280b3ef76e7a
Tim Stone <tim.stone@environment-agency.gov.uk>
2015-02-23 09:27:22 +0000

    pages update and IR AD test steps updated

--- features/support/pages/order_page.rb
+++ features/support/pages/order_page.rb
@@ -1,18 +1,20 @@
 module OrderPage
-  def complete_order_pay_by_world_pay_and_submit(number)
+  def order_page_complete_order_pay_by_world_pay_and_submit(number)
     fill_in 'registration_copy_cards', with: number
     choose 'registration_payment_type_world_pay'
     click_button 'proceed_to_payment'
   end
 
-  def complete_order_pay_by_bank_transfer_and_submit(number)
+  def order_page_complete_order_pay_by_bank_transfer_and_submit(number)
     fill_in 'registration_copy_cards', with: number
     choose 'registration_payment_type_bank_transfer'
     click_button 'proceed_to_payment'
   end
 
-  def complete_order_pay_check_total_charge(amount:'105.00')
-   totalChargeAmount = find(:xpath, '//*[@id="registration_registration_fee"]').text
+  def order_page_complete_order_pay_check_total_charge(amount:'105.00')
+   # totalChargeAmount = find(:xpath, '//*[@id="registration_registration_fee"]').text
+   totalChargeAmount = find(:xpath, '//*[@id="registration_total_fee"]').text
+   # puts totalChargeAmount
    totalChargeAmount.should match "#{amount}"
   end
 
