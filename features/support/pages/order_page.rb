b8abf9373e1927771a325daf79769d0572e857e4
Tim Stone <tim.stone@environment-agency.gov.uk>
2015-02-27 16:10:12 +0000

    updated AD IR tests

--- features/support/pages/order_page.rb
+++ features/support/pages/order_page.rb
@@ -1,20 +1,27 @@
 module OrderPage
-  def complete_order_pay_by_world_pay_and_submit(number)
+  def order_page_pay_by_world_pay_and_submit(number)
     fill_in 'registration_copy_cards', with: number
     choose 'registration_payment_type_world_pay'
     click_button 'proceed_to_payment'
   end
 
-  def complete_order_pay_by_bank_transfer_and_submit(number)
+  def order_page_pay_by_bank_transfer_and_submit(number)
     fill_in 'registration_copy_cards', with: number
     choose 'registration_payment_type_bank_transfer'
     click_button 'proceed_to_payment'
   end
 
-  def complete_order_pay_check_total_charge(amount:'105.00')
+  def order_page_check_registration_fee(amount:'105.00')
    totalChargeAmount = find(:xpath, '//*[@id="registration_registration_fee"]').text
    expect(totalChargeAmount).to have_text "#{amount}"
   end
+
+  def order_page_check_total_charge(amount:'105.00')
+   totalChargeAmount = find(:xpath, '//*[@id="registration_total_fee"]').text
+   pp totalChargeAmount
+   expect(totalChargeAmount).to have_text "#{amount}"
+  end
+ 
 
  def order_page_enter_copy_cards(no_of_cards: 3)
    fill_in 'registration_copy_cards', with: no_of_cards.to_i
