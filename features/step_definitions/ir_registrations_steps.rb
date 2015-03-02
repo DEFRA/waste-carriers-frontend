c4892c27ed6cf6b490345734f260277ff4ed8c75
Tim Stone <tim.stone@environment-agency.gov.uk>
2015-03-02 15:47:46 +0000

    Test updated should pass when #83644968 'IR renewals using NCCC login result in incorrect charge' is resolved

--- features/step_definitions/ir_registrations_steps.rb
+++ features/step_definitions/ir_registrations_steps.rb
@@ -38,7 +38,7 @@ When(/^I am registering an IR registration for a Public body and pay by credit c
   relevant_people_page_enter_multiple_convicted_people_and_submit
 end
 
-Given(/^I am registering an IR registration for a Partnership and pay by bank transfter$/) do
+Given(/^I am registering an IR registration for a Partnership and pay by bank transfer$/) do
   existing_registration_page_enter_partnership_registration_number
   business_type_page_submit
   other_businesses_page_select_yes
@@ -51,11 +51,68 @@ Given(/^I am registering an IR registration for a Partnership and pay by bank tr
   relevant_convictions_page_select_no
 end
 
+Given(/^I am registering an IR registration for a limited company with convictions and pay by bank transfer$/) do
+  existing_registration_page_enter_limited_company_registration_number
+  business_type_page_submit
+  other_businesses_page_select_yes
+  service_provided_page_select_yes
+  only_deal_with_page_select_no
+  registration_type_page_submit
+  business_details_page_enter_business_or_organisation_details_postcode_lookup_and_submit
+  contact_details_page_enter_ad_contact_details_and_submit
+  enter_key_people_details_and_submit
+  relevant_convictions_page_select_yes
+  relevant_people_page_enter_multiple_convicted_people_and_submit
+end
+
+Given(/^I am registering an IR registration for a limited company changing waste carrier type and pay by credit card$/) do
+  existing_registration_page_enter_limited_company_registration_number
+  business_type_page_submit
+  other_businesses_page_select_yes
+  service_provided_page_select_yes
+  only_deal_with_page_select_no
+  registration_type_page_select_carrier_dealer
+  business_details_page_enter_business_or_organisation_details_postcode_lookup_and_submit
+  contact_details_page_enter_ad_contact_details_and_submit
+  enter_key_people_details_and_submit
+  relevant_convictions_page_select_no
+end
+
+Given(/^I am registering an IR registration for a Sole trader changing waste carrier type with convictions and pay by bank transfer$/) do
+  existing_registration_page_enter_sole_trader_registration_number
+  business_type_page_submit
+  other_businesses_page_select_yes
+  service_provided_page_select_yes
+  only_deal_with_page_select_no
+  registration_type_page_select_carrier_dealer
+  business_details_page_enter_business_or_organisation_details_postcode_lookup_and_submit
+  contact_details_page_enter_ad_contact_details_and_submit
+  enter_key_people_details_and_submit
+  relevant_convictions_page_select_yes
+  relevant_people_page_enter_multiple_convicted_people_and_submit
+end
+
 Then(/^a renewal fee will be charged$/) do
   confirmation_page_registration_check_for_renewal_text
   confirmation_page_registration_and_submit
   order_page_enter_copy_cards(no_of_cards:0)
   order_page_check_total_charge(amount:'154.00')
+end
+
+Then(/^there will be a renewal and edit amount charged$/) do
+    confirmation_page_registration_check_for_renewal_text
+  confirmation_page_registration_and_submit
+  order_page_enter_copy_cards(no_of_cards:0)
+  #renewal fee is £105 plus £30 edit charge
+  order_page_check_total_charge(amount:'154.00')
+end
+
+Then(/^the correct renewal charge should be shown$/) do
+  finish_assisted_page_check_charge_amount(amount:'135.00')
+end
+
+Then(/^the correct renewal and edit charge should be shown$/) do
+  finish_assisted_page_check_charge_amount(amount:'135.00')
 end
 
 Then(/^the callers registration should be complete when payment is successful$/) do
