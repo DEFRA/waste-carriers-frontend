9c6915643f22edce93741d4089877a88c9f30078
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-03-05 01:25:02 +0000

    Replaced old should syntax with expect syntax. Also resolved a number of rubocop issues.

--- features/step_definitions/ir_registrations_steps.rb
+++ features/step_definitions/ir_registrations_steps.rb
@@ -1,9 +1,7 @@
 Given(/^I am renewing an IR registration$/) do
-
   # Manually force a repopulation of IR data in database
-  RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/ir-repopulate', :content_type => :json, :accept => :json
-  #sleep 1.0			# Wait a period for background task of pre-population to occur
-
+  RestClient.post Rails.configuration.waste_exemplar_services_admin_url +
+    '/tasks/ir-repopulate', content_type: :json, accept: :json
   visit start_path
   choose 'registration_newOrRenew_renew'
   click_button 'continue'
@@ -12,9 +10,9 @@ Given(/^I am renewing an IR registration$/) do
 end
 
 Given(/^I have chosen to renew my registration from IR$/) do
- # Manually force a repopulation of IR data in database
-  RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/ir-repopulate', :content_type => :json, :accept => :json
-  #sleep 1.0      # Wait a period for background task of pre-population to occur  
+  # Manually force a repopulation of IR data in database
+  RestClient.post Rails.configuration.waste_exemplar_services_admin_url +
+    '/tasks/ir-repopulate', content_type: :json, accept: :json
   go_to_start_page
   start_page_select_renew
 end
@@ -157,8 +155,8 @@ end
 Then(/^a renewal fee will be charged$/) do
   confirmation_page_registration_check_for_renewal_text
   confirmation_page_registration_and_submit
-  order_page_enter_copy_cards(no_of_cards:0)
-  order_page_check_total_charge(amount:'105.00')
+  order_page_enter_copy_cards(no_of_cards: 0)
+  order_page_check_total_charge(amount: '105.00')
 end
 
 Then(/^I will be charged a renewal fee$/) do
@@ -166,28 +164,27 @@ Then(/^I will be charged a renewal fee$/) do
   confirmation_page_registration_and_submit
   enter_email_details_and_submit
   # order_page_enter_copy_cards(no_of_cards:0)
-  order_page_check_total_charge(amount:'105.00')
+  order_page_check_total_charge(amount: '105.00')
 end
-
 
 Then(/^there will be a renewal and edit amount charged$/) do
   confirmation_page_registration_check_for_renewal_text
   confirmation_page_registration_and_submit
-  order_page_enter_copy_cards(no_of_cards:0)
-  #renewal fee is £105 plus £30 edit charge
-  order_page_check_total_charge(amount:'135.00')
+  order_page_enter_copy_cards(no_of_cards: 0)
+  # renewal fee is 105 plus 30 edit charge
+  order_page_check_total_charge(amount: '135.00')
 end
 
 Then(/^the correct renewal charge should be shown$/) do
-  finish_assisted_page_check_charge_amount(amount:'135.00')
+  finish_assisted_page_check_charge_amount(amount: '135.00')
 end
 
 Then(/^my correct renewal charge should be shown$/) do
-  confirmed_page_check_charge_amount(amount:'105.00')
+  confirmed_page_check_charge_amount(amount: '105.00')
 end
 
 Then(/^the correct renewal and edit charge should be shown$/) do
-  finish_assisted_page_check_charge_amount(amount:'135.00')
+  finish_assisted_page_check_charge_amount(amount: '135.00')
 end
 
 Then(/^the callers registration should be complete when payment is successful$/) do
@@ -216,7 +213,8 @@ end
 
 Given(/^I have completed smart answers given my existing IR data$/) do
   # Prepopulated business type
-  #choose 'registration_businessType_soletrader'  # assume pre pop business type
+  # choose 'registration_businessType_soletrader'
+  # assume pre pop business type
   click_button 'continue'
 
   # Remaining smart answer questions are not prepopulated
@@ -230,7 +228,9 @@ end
 
 Given(/^I have completed smart answers, but changed my business type$/) do
   # Prepopulated business type
-  choose 'registration_businessType_partnership'  # assumes original value was soletrader, thus partnership is a change from original
+  # assumes original value was soletrader, thus partnership is a change from
+  # original
+  choose 'registration_businessType_partnership'
   click_button 'continue'
 
   # Remaining smart answer questions are not prepopulated
@@ -251,7 +251,7 @@ Given(/^my company name is prepopulated$/) do
 end
 
 Then(/^registration should be complete$/) do
-  page.should have_content 'Registration complete'
+  expect(page).to have_text 'Registration complete'
 end
 
 Then(/^the callers registration should be complete$/) do
@@ -263,11 +263,12 @@ Then(/^registration should be pending convictions checks$/) do
 end
 
 Then(/^the callers registration should be pending convictions checks$/) do
-  page.should have_content 'The applicant declared relevant people with convictions'
+  expect(page).to have_text 'The applicant declared relevant people with '\
+                            'convictions'
 
   # validate the access code is present and of the correct length
   access_code = page.find_by_id 'accessCode'
-  access_code.text.length.should == 6
+  expect(access_code.text.length).to eq(6)
 end
 
 Then(/^registration should be pending payment$/) do
@@ -284,15 +285,15 @@ Then(/^my registration should be pending payment$/) do
   confirmed_page_check_pending_payment_text
 end
 
-
 Given(/^I have signed in as an Environment Agency user$/) do
   sign_in_page_sign_in_as_environment_agency_user_and_submit
 end
 
 Given(/^have chosen to renew a customers existing licence$/) do
-  RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/ir-repopulate', :content_type => :json, :accept => :json
-   registrations_page_select_new_registration
-   start_page_select_renew
+  RestClient.post Rails.configuration.waste_exemplar_services_admin_url +
+    '/tasks/ir-repopulate', content_type: :json, accept: :json
+  registrations_page_select_new_registration
+  start_page_select_renew
 end
 
 When(/^I only change business details$/) do
@@ -300,8 +301,10 @@ When(/^I only change business details$/) do
   other_businesses_page_select_no
   construction_demolition_page_select_yes
   registration_type_page_submit
-  business_details_page_enter_ltd_business_details_postcode_lookup_and_submit(companyNo: '07713745',postcode: 'BS1 5AH',
-  address: 'HARMSEN GROUP, TRIODOS BANK, DEANERY ROAD, BRISTOL, BS1 5AH')
+  business_details_page_enter_ltd_business_details_postcode_lookup_and_submit(
+    companyNo: '07713745',
+    postcode: 'BS1 5AH',
+    address: 'HARMSEN GROUP, TRIODOS BANK, DEANERY ROAD, BRISTOL, BS1 5AH')
   contact_details_page_enter_ad_contact_details_and_submit
   enter_key_people_details_and_submit
   relevant_convictions_page_select_no
@@ -309,16 +312,17 @@ When(/^I only change business details$/) do
 end
 
 When(/^I make no other changes to my registration details$/) do
-# noop (step exists for readability of the feature only)
+  # noop (step exists for readability of the feature only)
 end
-
 
 When(/^I only change business name$/) do
   business_type_page_submit
   other_businesses_page_select_no
   construction_demolition_page_select_yes
   registration_type_page_submit
-  business_details_page_enter_ltd_business_details_postcode_lookup_and_submit(companyNo: '07713745',companyName: 'New company name')
+  business_details_page_enter_ltd_business_details_postcode_lookup_and_submit(
+    companyNo: '07713745',
+    companyName: 'New company name')
   contact_details_page_enter_ad_contact_details_and_submit
   enter_key_people_details_and_submit
   relevant_convictions_page_select_no
@@ -334,20 +338,19 @@ When(/^I change business details$/) do
 end
 
 Given(/^I don't change waste carrier type$/) do
-  click_button 'continue' 
+  click_button 'continue'
 end
 
 When(/^I Enter their contact details$/) do
-
-    fill_in 'registration_firstName', with: 'Joe'
-    fill_in 'registration_lastName', with: 'Bloggs'
-    fill_in 'registration_phoneNumber', with: '0117 926 8332'
-    click_button 'continue'
+  fill_in 'registration_firstName', with: 'Joe'
+  fill_in 'registration_lastName', with: 'Bloggs'
+  fill_in 'registration_phoneNumber', with: '0117 926 8332'
+  click_button 'continue'
 end
 
 When(/^I confirm their details$/) do
-    check 'registration_declaration'
-    click_button 'confirm'
+  check 'registration_declaration'
+  click_button 'confirm'
 end
 
 Then(/^I should be shown the total cost is the charge amount and renewal amount "(.*?)"$/) do |arg1|
@@ -359,23 +362,24 @@ Given(/^I change business type to Sole Trader$/) do
   click_button 'continue'
 end
 
- Then(/^I should be told that I have to start a new registration$/) do
-  page.should have_content 'new upper tier registration will be made and will require a payment of £154.00'
+Then(/^I should be told that I have to start a new registration$/) do
+  expect(page).to have_text 'new upper tier registration will be made and '\
+                            'will require a payment of £154.00'
 end
 
 Given(/^have chosen to renew an existing licence$/) do
-   RestClient.post Rails.configuration.waste_exemplar_services_admin_url + '/tasks/ir-repopulate', :content_type => :json, :accept => :json
-   visit start_path
-   start_page_select_renew
+  RestClient.post Rails.configuration.waste_exemplar_services_admin_url +
+    '/tasks/ir-repopulate', content_type: :json, accept: :json
+  visit start_path
+  start_page_select_renew
 end
 
 Given(/^I don't change business type$/) do
   click_button 'continue'
-
 end
 
 Given(/^the smart answers keep me in Upper tier$/) do
-   choose 'registration_otherBusinesses_no'
+  choose 'registration_otherBusinesses_no'
   click_button 'continue'
   choose 'registration_constructionWaste_yes'
   click_button 'continue'
@@ -394,21 +398,21 @@ When(/^I Enter business details$/) do
 end
 
 When(/^I Enter my contact details$/) do
-    fill_in 'registration_firstName', with: 'Joe'
-    fill_in 'registration_lastName', with: 'Bloggs'
-    fill_in 'registration_phoneNumber', with: '0117 926 8332'
-    fill_in 'registration_contactEmail', with: my_email_address
-    click_button 'continue'
+  fill_in 'registration_firstName', with: 'Joe'
+  fill_in 'registration_lastName', with: 'Bloggs'
+  fill_in 'registration_phoneNumber', with: '0117 926 8332'
+  fill_in 'registration_contactEmail', with: my_email_address
+  click_button 'continue'
 end
 
 When(/^I Enter key people details$/) do
-    fill_in 'key_person_first_name', with: 'Joe'
-    fill_in 'key_person_last_name', with: 'Bloggs'
-    fill_in 'key_person_dob_day', with: '01'
-    fill_in 'key_person_dob_month', with: '02'
-    fill_in 'key_person_dob_year', with: '1980'
-    click_button 'add_btn'
-    click_button 'continue'
+  fill_in 'key_person_first_name', with: 'Joe'
+  fill_in 'key_person_last_name', with: 'Bloggs'
+  fill_in 'key_person_dob_day', with: '01'
+  fill_in 'key_person_dob_month', with: '02'
+  fill_in 'key_person_dob_year', with: '1980'
+  click_button 'add_btn'
+  click_button 'continue'
 end
 
 When(/^I have no relevant convictions$/) do
@@ -420,22 +424,23 @@ When(/^I confirm my details$/) do
   check 'registration_declaration'
   click_button 'confirm'
 
-   fill_in 'registration_accountEmail', with: my_email_address
+  fill_in 'registration_accountEmail', with: my_email_address
   fill_in 'registration_accountEmail_confirmation', with: my_email_address
   fill_in 'registration_password', with: my_password
   fill_in 'registration_password_confirmation', with: my_password
   click_button 'continue'
 end
 
-
 Then(/^I should be shown the total cost "(.*?)"$/) do |amount|
-  registration_total_fee = page.find(:xpath, ".//input[@id='registration_total_fee']").value
+  registration_total_fee = page.find(
+    :xpath,
+    ".//input[@id='registration_total_fee']").value
   assert_equal amount, registration_total_fee
 end
 
 Then(/^have the option to pay by Credit or Debit card or by bank transfer$/) do
-  page.should have_button 'worldpay_button'
-  page.should have_button 'offline_pay_button'
+  expect(page).to have_button 'worldpay_button'
+  expect(page).to have_button 'offline_pay_button'
 end
 
 Given(/^I am renewing a valid IR registration for sole trader$/) do
@@ -446,21 +451,21 @@ Given(/^I am renewing a valid IR registration for sole trader$/) do
 end
 
 When(/^I change waste carrier type from CD to CBD$/) do
- choose 'registration_registrationType_carrier_broker_dealer'
- click_button 'continue'
+  choose 'registration_registrationType_carrier_broker_dealer'
+  click_button 'continue'
 end
 
 When(/^I Enter my details$/) do
-     fill_in 'key_person_first_name', with: 'Joe'
-    fill_in 'key_person_last_name', with: 'Bloggs'
-    fill_in 'key_person_dob_day', with: '01'
-    fill_in 'key_person_dob_month', with: '02'
-    fill_in 'key_person_dob_year', with: '1980'
-    click_button 'continue'
+  fill_in 'key_person_first_name', with: 'Joe'
+  fill_in 'key_person_last_name', with: 'Bloggs'
+  fill_in 'key_person_dob_day', with: '01'
+  fill_in 'key_person_dob_month', with: '02'
+  fill_in 'key_person_dob_year', with: '1980'
+  click_button 'continue'
 end
 
 Given(/^I am renewing a valid CBD IR registration for Partnership$/) do
-    choose 'registration_newOrRenew_renew'
+  choose 'registration_newOrRenew_renew'
   click_button 'continue'
   fill_in 'registration_originalRegistrationNumber', with: 'CB/AN8888ZZ/R002'
   click_button 'continue'
@@ -472,7 +477,7 @@ When(/^I change waste carrier type from CBD to BD$/) do
 end
 
 Given(/^I am renewing a valid CD IR registration for Public Body$/) do
-      choose 'registration_newOrRenew_renew'
+  choose 'registration_newOrRenew_renew'
   click_button 'continue'
   fill_in 'registration_originalRegistrationNumber', with: 'CB/VM8888WW/A001'
   click_button 'continue'
@@ -484,25 +489,24 @@ When(/^I change waste carrier type from CD to BD$/) do
 end
 
 Given(/^I am renewing a valid CBD IR registration for limited company$/) do
-   existing_registration_page_enter_limited_company_registration_number
+  existing_registration_page_enter_limited_company_registration_number
 end
 
 When(/^I change waste carrier type from BD to CD$/) do
-   choose 'registration_registrationType_carrier_dealer'
-    click_button 'continue'
+  choose 'registration_registrationType_carrier_dealer'
+  click_button 'continue'
 end
 
-
 Given(/^I am renewing a valid BD IR registration for Partnership$/) do
-     choose 'registration_newOrRenew_renew'
+  choose 'registration_newOrRenew_renew'
   click_button 'continue'
   fill_in 'registration_originalRegistrationNumber', with: 'CB/AN8888ZZ/R003'
   click_button 'continue'
 end
 
 When(/^I change waste carrier type from BD to CBD$/) do
-   choose 'registration_registrationType_carrier_broker_dealer'
- click_button 'continue'
+  choose 'registration_registrationType_carrier_broker_dealer'
+  click_button 'continue'
 end
 
 Then(/^registration should be complete when payment is successful$/) do
@@ -512,4 +516,3 @@ Then(/^registration should be complete when payment is successful$/) do
   secure_test_simulator_page_continue
   confirmed_page_check_text_registration_complete
 end
-
--- features/support/pages/confirmation_page.rb
+++ features/support/pages/confirmation_page.rb
@@ -1,16 +1,16 @@
+# Helper used in testing for working with the confirmation page.
 module ConfirmationPage
-  def confirmation_page_registration_and_submit()
+  def confirmation_page_registration_and_submit
     check 'registration_declaration'
     confirmation_page_submit
   end
 
-   def confirmation_page_registration_check_for_renewal_text()
-    page.should have_content 'Renewing your registration'
+  def confirmation_page_registration_check_for_renewal_text
+    expect(page).to have_text 'Renewing your registration'
   end
 
-  def confirmation_page_submit()
+  def confirmation_page_submit
     click_button 'confirm'
   end
 end
 World(ConfirmationPage)
-
