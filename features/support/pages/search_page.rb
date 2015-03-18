567b5edd733443c34e74180760abb680375f9e7e
Tom Thurston <tomthurstonwork@live.com>
2015-03-18 17:16:18 +0000

    [#89898502] Slight refactoring tests to use preferred method of checking Public Register search results.

--- features/support/pages/search_page.rb
+++ features/support/pages/search_page.rb
@@ -1,17 +1,17 @@
 module SearchPage
-  def search_page_search(companyName: 'Find me in the public register')
-  	sleep 5.0
-  	visit public_path
-    fill_in 'q', with: companyName
-    click_button 'reg-search'
-   end
-
-  def search_page_check_search_result_positive
-  	expect(page).to have_no_content('0 registrations found')
+  def search_page_check_search_result_positive(searchTerm)
+    visit public_path
+    waitForSearchResultsToContainText(
+    searchTerm,
+      'Showing 1 of 1')
 	end
 
-  def search_page_check_search_result_negative
-  	expect(page).to have_content('0 registrations found')
+  def search_page_check_search_result_negative(searchTerm)
+    visit public_path
+    waitForSearchResultsToContainText(
+    searchTerm,
+      'Showing 0 of 0')
 	end
 end
+
 World(SearchPage)
