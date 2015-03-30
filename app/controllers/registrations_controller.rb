791895ebc01af8ef9eb70d66161a0e9bd8fc430c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-03-30 16:22:03 +0100

    Updated routing in updateNewConfirmation action. If payment is outstanding we route logged in users to the payment page, else return them to the start for no chargeable edits. Also sorted a series of rubocop issues but essentially this needs a thorough refactoring [#91265798].

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -410,63 +410,54 @@ class RegistrationsController < ApplicationController
         redirect_to upper_payment_path(@registration.uuid) and return
       when EditMode::EDIT
         case session[:edit_result].to_i
-        # Check if no Immediate edit actions have occured, and redirect back to appropraite start point
-        # This assumes that because the edit_result orignally is set to the start, and then when newConfirmation is
-        # rendered it is incremented by one, that this is the only situation where that can occur.
+        # Check if no Immediate edit actions have occured, and redirect back to
+        # appropraite start point
+        # This assumes that because the edit_result orignally is set to the
+        # start, and then when newConfirmation is rendered it is incremented by
+        # one, that this is the only situation where that can occur.
         when  EditResult::START + 1
-          clear_edit_session # we don't need edit variables polluting the session any more
+          unless @registration.paid_in_full?
+            newOrder @registration.uuid
+            return
+          end
+          clear_edit_session
           if current_user
             redirect_to userRegistrations_path(current_user)
           elsif current_agency_user
-            redirect_to :action => 'index'
+            redirect_to registrations_path
           else
             renderAccessDenied
           end
+          return
         when  EditResult::NO_CHANGES, EditResult::UPDATE_EXISTING_REGISTRATION_NO_CHARGE
-          if @registration.save!
-            logger.debug "Registration #{@registration.uuid} now saved!"
-          else
-            #TODO: error handling
-          end #if
-          edit_mode = session[:edit_mode]
-          edit_result = session[:edit_result]
-          clear_edit_session # we don't need edit variables polluting the session any more
-          # redirect_to(action: 'editRenewComplete', edit_mode: edit_mode, edit_result: edit_result) and return
+          @registration.save!
+          unless @registration.paid_in_full?
+            newOrder @registration.uuid
+            return
+          end
+          clear_edit_session
           if current_user
             redirect_to userRegistrations_path(current_user)
-          else
+          elsif current_agency_user
             redirect_to registrations_path
+          else
+            renderAccessDenied
           end
           return
         when  EditResult::UPDATE_EXISTING_REGISTRATION_WITH_CHARGE
           newOrderEdit @registration.uuid
         when  EditResult::CREATE_NEW_REGISTRATION
-          # If a new registration is needed at this point it should be created as the payment will they be processed aggainst that registration and
+          # If a new registration is needed at this point it should be created
+          # as the payment will they be processed aggainst that registration and
           # not the original one, which should be marked as deleted
 
-          #
-          # TODO::
-          # :Make a copy of the registration in memory (create a new local)
+          # TODO: Make a copy of the registration in memory (create a new local)
           newRegistration = Registration.create
           session[:editing] = true
 
-          logger.info 'before: ' + newRegistration.to_json
-          logger.info 'before: ' + newRegistration.attributes.to_s
-
-          newRegistration.add( @registration.to_hash )
-          newRegistration.add( @registration.attributes )
-
-          logger.info ''
-          logger.info '================='
-          logger.info '==== NEW REGISTRAION ============='
-          logger.info 'after: ' + newRegistration.to_json.to_s
-          logger.info 'after: ' + newRegistration.attributes.to_s
-          logger.info '================='
-          logger.info ''
-
-          # Need to re-get registration from DB as we are leaving the original alone
+          # Need to re-get registration from DB as we are leaving the orig alone
           originalRegistration = Registration.find_by_id(@registration.uuid)
-          # :Mark original registration as deleted and save to db
+          # Mark original registration as deleted and save to db
           originalRegistration.metaData.first.update(:status=>'INACTIVE')
           logger.info 'updated meta data, about to save to db'
 
@@ -475,8 +466,7 @@ class RegistrationsController < ApplicationController
             originalRegistration.save
           end
 
-
-          # :Use copy of registration in memory and save to database
+          # Use copy of registration in memory and save to database
           logger.info 'Check if newRegistraiton is valid'
           newRegistration.current_step = 'confirmation'
           if newRegistration.valid?
@@ -489,59 +479,48 @@ class RegistrationsController < ApplicationController
             else
               logger.info 'newRegistration failed to commit'
               # return to new Confirmation
-              redirect_to :newConfirmation and return
+              redirect_to :newConfirmation && return
             end
           else
             logger.info 'newRegistration not valid.'
             # return to new Confirmation
-            redirect_to :newConfirmation and return
+            redirect_to :newConfirmation && return
           end
 
-          # :Save copied registration to redis and update any session variables
+          # Save copied registration to redis and update any session variables
           @registration.save
           logger.info 'new reg saved to db now save to redis: ' + @registration.id
 
-          #
-          #logger.info 'Registration is not valid, and data is not yet saved'
-          #render "newConfirmation", :status => '400'
-
-          #redirect_to newOrderCausedNew_path(@registration.uuid) and return
           logger.info '@registration.uuid:' + @registration.uuid
           newOrderCausedNew @registration.uuid
-          #newOrderEdit @registration.uuid
         else
           edit_mode = session[:edit_mode]
           edit_result = session[:edit_result]
           clear_edit_session # we don't need edit variables polluting the session any more
-          redirect_to(action: 'editRenewComplete', edit_mode: edit_mode, edit_result: edit_result) and return
+          redirect_to(
+            action: 'editRenewComplete',
+            edit_mode: edit_mode,
+            edit_result: edit_result) && return
         end
 
       when EditMode::RENEWAL
-
         # Detect standard or IR renewal
         if @registration.originalRegistrationNumber and isIRRegistrationType(@registration.originalRegistrationNumber) and @registration.newOrRenew
-
-          logger.debug "Is IR RENEWAL"
-
-          # ir renewal detected
           redirect_to :action => :account_mode
         else
-
-          logger.debug "Is normal RENEWAL"
-
-          @registration.renewalRequested = true;
+          @registration.renewalRequested = true
 
           @registration.save
-          newOrderRenew(@registration.uuid) and return
+          newOrderRenew(@registration.uuid) && return
         end
       else # new registration
         redirect_to :action => :account_mode
-      end #case
+      end
 
     else
       # there is an error (but data not yet saved)
       logger.info 'Registration is not valid, and data is not yet saved'
-      render "newConfirmation", :status => '400'
+      render 'newConfirmation', status: '400'
     end
   end
 
