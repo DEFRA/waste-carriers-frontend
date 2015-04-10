4a4b12261ecb6b4d2ccbc49dfb34e0897b2ec585
Tom Thurston <tomthurstonwork@live.com>
2015-04-10 11:47:14 +0100

    [#91483940] Bonus functionality!  Also allow users to order copy cards when their Edit causes a new registration to be created.

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -18,6 +18,7 @@ module OrderHelper
   def show_copy_cards?(render_type)
     render_type.eql?(Order.new_registration_identifier) ||
       render_type.eql?(Order.renew_registration_identifier) ||
+      render_type.eql?(Order.editrenew_caused_new_identifier) ||
       render_type.eql?(Order.extra_copycards_identifier)
   end
 
