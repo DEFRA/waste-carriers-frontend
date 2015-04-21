0c660349a978cbb484d6cd28896aa92bd3cf22de
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-04-21 21:26:23 +0100

    Updated location model to hold easting and northing, plus added missing ohm helper methods [#92511498]

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,19 +1,20 @@
+# Holds additional location details for the business address on a registration
 class Location < Ohm::Model
-
   attribute :lat
   attribute :lon
+  attribute :easting
+  attribute :northing
 
-
-  # Creates a new Metadata object from a metadata-formatted hash
+  # Creates a new Location object from a location formatted hash
   #
-  # @param md_hash [Hash] the metadata-formatted hash
+  # @param location_hash [Hash] the location formatted hash
   # @return [Location] the Ohm-derived Location object.
   class << self
-    def init (location_hash)
+    def init(location_hash)
       loc = Location.create
 
       location_hash.each do |k, v|
-          loc.send(:update, {k.to_sym => v})
+        loc.send(:update, k.to_sym => v)
       end
 
       loc.save
@@ -21,5 +22,18 @@ class Location < Ohm::Model
     end
   end
 
+  # Returns a hash representation of the Location object.
+  # @param none
+  # @return  [Hash]  the Location object as a hash
+  def to_hash
+    self.attributes.to_hash
+  end
 
+  # Returns a JSON Java/DropWizard API compatible representation of the
+  # Location object.
+  # @param none
+  # @return  [String]  the Location object in JSON form
+  def to_json
+    to_hash.to_json
+  end
 end
