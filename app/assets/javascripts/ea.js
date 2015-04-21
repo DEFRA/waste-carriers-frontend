91bd6c4cfafc8532f4611275b6c8c9565a338556
Tom Thurston <tomthurstonwork@live.com>
2015-04-21 16:42:43 +0100

    Remove obsolete address-related (and other) functions from the main Javascript file.

--- app/assets/javascripts/ea.js
+++ app/assets/javascripts/ea.js
@@ -4,119 +4,7 @@
 
 	var page = 1;
 
-	var addressLookup = {};
-
-	var addressTextLookup = {};
-
 	var advancedSearch = false;
-
-	var useManualAddress;
-
-	var orgTypeLookup = {
-			"individual":"individual",
-			"organisationOfIndividuals":"organisation of individuals",
-			"limitedCompany":"limited company or limited liability partnership",
-			"publicBody":"public body"
-		};
-
-	function findAddress(){
-		var value = $("#registration_postcodeSearch").val();
-		if(value == "")value = "MK5 7TQ";
-
-		var $addresses = $("#addresses");
-		$addresses.html("");
-		for(var i=0; i<6; i++){
-			var addr;
-			var addrValue;
-			if(i==0){
-				addr = "Please select an address..";
-				addrValue = "";
-			}else{
-				addr = (i+5)+" Some street, Some town, "+value;
-				addrValue = "addr"+i;
-			}
-
-			var option = new Option(addr,addrValue);
-			$(option).html(addr);
-			$addresses.append(option);
-
-			addressTextLookup[addrValue] = addr;
-			addressLookup[addrValue] = {houseNumber:i+5, streetLine1:"Some street",streetLine2:"",townCity:"Some town",postcode:value};
-		}
-
-		$("#addressSearchResults").css("display","");
-	}
-
-	function updateAddress(){
-		var address = $("#addresses").val();
-		if(address==="")return;
-
-		var addressObject = addressLookup[address];
-		$("#registration_houseNumber").val(addressObject.houseNumber);
-		$("#registration_streetLine1").val(addressObject.streetLine1);
-		$("#registration_streetLine2").val(addressObject.streetLine2);
-		$("#registration_townCity").val(addressObject.townCity);
-		$("#registration_postcode").val(addressObject.postcode);
-
-		setHidden("uprn",address);
-		setHidden("address",addressTextLookup[address]);
-
-	}
-
-	function manualAddress(){
-		$("#addressSearchResults").css("display","none");
-		$("#selectedAddress").css("display","none");
-		$("input[name=findAddress]").css("display","none");
-		$("#addressManual").css("display","");
-		$("#addressSearch").css("display","");
-		setHidden("uprn","");
-		setHidden("address","");
-		useManualAddress = true;
-	}
-
-
-	function submitAddress(){
-		if(!useManualAddress){
-			return;
-		}
-		var address = $("#registration_houseNumber").val() + " " + $("#registration_streetLine1").val();
-		var address2 = $("#registration_streetLine2").val();
-		if(address2!=""){
-			address += ", "+address2;
-		}
-		var city = $("#registration_townCity").val();
-		if(city!=""){
-			address += ", "+city;
-		}
-		address += ", "+$("#registration_postcode").val();
-		setHidden("address",address);
-	}
-
-	function changeAddress(){
-		$("#selectedAddress").css("display","none");
-		$("#addressSearch").css("display","");
-		setHidden("uprn","");
-		setHidden("address","");
-	}
-
-	function updateSummary(){
-		var address = $("#addresses").val();
-		address = addressLookup[address];
-
-		var data = {
-			organisationType: $("#registration_businessType").val(),
-			organisationName: $("#registration_companyName").val(),
-			firstName : $("#registration_firstName").val(),
-			lastName : $("#registration_lastName").val(),
-			emailAddress : $("#registration_emailAddress").val(),
-			phoneNumber: $("#registration_phoneNumber").val(),
-			individualsType: $("#registration_individualsType").val(),
-			publicBodyType:$("#registration_publicBodyType").val(),
-			address: address
-		}
-
-		updateSummaryWithData(data,$("#summary").get(0));
-	}
 
 	function searchResultSummaries(){
 		$("#reg-search-result .detail .data").each(function(index,elem){
@@ -208,9 +96,6 @@
 	}
 
 	function authorViewDetail(elem,a){
-
-
-
 		var shown = true;
 		return function(e){
 			// reset all details to hidden
@@ -265,68 +150,14 @@
 		$(elem).html(html);
 	}
 
-	function refreshQuestions(){
-		var value = $("#registration_businessType").val();
-		var publicBody = $("#registration_publicBodyType").val();
-
-		$("#indType-wrapper").css("display",value === "organisationOfIndividuals"?"":"none");
-		$("#regNumber-wrapper").css("display",value === "limitedCompany"?"":"none");
-		$("#publicBodyType-wrapper").css("display",value === "publicBody"?"":"none");
-		$("#publicBodyOther-wrapper").css("display",(value === "publicBody" && publicBody === "other")?"":"none");
-
-		if(value !== ""){
-			$("#orgTypeQuestions").removeClass("js-hidden");
-		}else{
-			$("#orgTypeQuestions").addClass("js-hidden");
-		}
-	}
-
-	function setHidden(key,value){
-		$("#registration_"+key).val(value);
-	}
-
-	function regSearch(){
-		var businessName = $("#companyName").val().toLowerCase();
-		var searchOrgType = $("#businessType").val().toLowerCase();
-		var searchPostcode = $("#postcode").val().toLowerCase();
-
-		var count = 0;
-		$("#reg-search-result .box").each(function(index,elem){
-			var $dataElem = $(".data",elem);
-			var name = $dataElem.attr("data-organisationName").toLowerCase();
-			var orgType = $dataElem.attr("data-organisationType").toLowerCase();
-			var postcode = $dataElem.attr("data-postcode").toLowerCase();
-
-			var pass = name.indexOf(businessName)!=-1;
-			if(advancedSearch){
-				pass = pass && (searchOrgType==="" || orgType.indexOf(searchOrgType)!=-1) && (searchPostcode==="" || postcode.indexOf(searchPostcode)!=-1)
-			}
-
-			if(pass){
-				count++;
-				$(elem).css("display","block");
-			}else{
-				$(elem).css("display","none");
-			}
-		});
-		if(count>0){
-			$("#reg-search-result > h2").html("Found "+count+" registrations");
-			$("#reg-search-result > p").html("Showing 1-"+count+" of "+count+" records");
-		}else{
-			$("#reg-search-result > h2").html("No matching registrations found");
-			$("#reg-search-result > p").html("");
-		}
-		$("#reg-search-result").css("display","block");
-	}
-
 	function advancedToggle(){
 		advancedSearch = !advancedSearch;
 
-		if(advancedSearch){
+		if (advancedSearch) {
 			var myFullLabel = $("#full-search-label").html();
 			$("a#toggle-search").html(myFullLabel);
 			$("#advanced-options").css("display","block");
-		}else{
+		} else {
 			var myRefineLabel = $("#refine-search-label").html();
 			$("a#toggle-search").html(myRefineLabel);
 			$("#advanced-options").css("display","none");
@@ -342,33 +173,6 @@
 			return true;
 		}
 		return false;
-	}
-
-	function toggleSignInUp(){
-		var signin = $('#registration_sign_up_mode').val() == "sign_in";
-		var signup = $('#registration_sign_up_mode').val() == "sign_up";
-		if(signup||signin) {
-			$('#registration_accountEmail').parent().removeClass("js-hidden");
-			$('#registration_password').parent().removeClass("js-hidden");
-			if(signup) {
-				$('#registration_accountEmail_confirmation').parent().removeClass("js-hidden");
-				$('#registration_password_confirmation').parent().removeClass("js-hidden");
-				$('#emailSignUpText').removeClass("js-hidden");
-				$('#accountExists').addClass("js-hidden");
-				$('#registration_accountEmail').removeAttr('readonly');
-
-			} else {
-				$('#registration_password_confirmation').parent().addClass("js-hidden");
-				$('#registration_accountEmail_confirmation').parent().addClass("js-hidden");
-				$('#emailSignUpText').addClass("js-hidden");
-				$('#accountExists').removeClass("js-hidden");
-			}
-		} else {
-			$('#registration_accountEmail').parent().addClass("js-hidden");
-			$('#registration_accountEmail_confirmation').parent().addClass("js-hidden");
-			$('#registration_password').parent().addClass("js-hidden");
-			$('#registration_password_confirmation').parent().addClass("js-hidden");
-		}
 	}
 
 	function showHideRevoke(){
@@ -456,7 +260,6 @@
 		});
 	}
 
-
 	$(document).ready(function(){
 		// Setup Revoke questions on NCCC edit
 		$("#revoke_question").change(function(e){showHideRevoke();});
@@ -467,6 +270,7 @@
 			e.preventDefault();
 			advancedToggle();
 		});
+		
 		$("#manualAddressLink").click(function(e){
 			e.preventDefault();
 			manualAddress();
