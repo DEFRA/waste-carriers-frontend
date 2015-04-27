8ccb3ee88b01b1c5e066672d2706f0c3d1d05c31
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-04-27 08:26:18 +0100

    Initial resolution of some rubocop violations.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,14 @@
+# Default controller for the service, it handles the index (root) page and
+# other pages like version and maintenance
 class HomeController < ApplicationController
 
   def index
-  	if Rails.application.config.show_developer_index_page
-  		#not redirecting - show the home_index_path
-  	else
-  		if is_admin_request?
-  		  redirect_to registrations_path
-  		else
+    if Rails.application.config.show_developer_index_page
+      # not redirecting - show the home_index_path
+    else
+      if is_admin_request?
+        redirect_to registrations_path
+      else
         if user_signed_in?
           redirect_to userRegistrations_path(current_user)
         elsif agency_user_signed_in?
@@ -17,12 +19,12 @@ class HomeController < ApplicationController
           if Rails.env.production?
             redirect_to find_path
           else
-            #not redirecting - show the developer index page...
+            # not redirecting - show the developer index page...
           end
         end
-  		end
-  	end
-  end #end index
+      end
+    end
+  end
 
   def version
     @railsVersion = Rails.configuration.application_version
@@ -30,17 +32,13 @@ class HomeController < ApplicationController
     begin
       @jenkins_build_number = File.read('jenkins_build_number')
     rescue
-      @jenkins_build_number = "not available"
+      @jenkins_build_number = 'not available'
     end
 
     # Request version from REST api
-    @apiVersionObj = Version.find(:one, :from => "/version.json" )
-    if !@apiVersionObj.nil?
-      logger.debug 'Version info, version number:' + @apiVersionObj.versionDetails + ' lastBuilt: ' + @apiVersionObj.lastBuilt
+    @apiVersionObj = Version.find(:one, from: '/version.json')
+    unless @apiVersionObj.nil?
       @apiVersion = @apiVersionObj.versionDetails
     end
-
-#    render :layout => false
   end
-
 end
