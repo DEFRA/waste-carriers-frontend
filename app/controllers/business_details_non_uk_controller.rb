8eaf714f2d2a9952e9972881ab915b5202c22dca
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-05-07 16:03:50 +0100

    Added separate non uk address entry controller and view [#92511498]

---
+++ app/controllers/business_details_non_uk_controller.rb
@@ -0,0 +1,53 @@
+class BusinessDetailsNonUkController < ApplicationController
+  include RegistrationsHelper
+
+  # GET /your-registration/business-details-non-uk
+  def show
+    new_step_action 'businessdetails'
+    @address = @registration.registered_address
+  end
+
+  # POST /your-registration/business-details-non-uk
+  def create
+    setup_registration 'businessdetails'
+    return unless @registration
+
+    # You must get the address object before the possibility of returning to the
+    # view as the view expects an instantiated address.
+    @address = @registration.registered_address
+
+    unless @registration.valid?
+      render 'show', status: '400'
+      return
+    end
+
+    # Clear the existing address and then update it based on the mode we're in
+    # and the data posted from the form
+    @address.clear
+    @address.update_attributes(address_mode: 'manual-foreign')
+    @address.update_attributes(params[:address])
+    @address.save
+
+    unless @address.valid?
+      render 'show', status: '400'
+      return
+    end
+
+    # If the registration is upper tier we run a conviction check against
+    # the company name.
+    unless @registration.tier.eql? 'LOWER'
+      @registration.cross_check_convictions
+      @registration.save
+    end
+
+    redirect_to :newContact
+  end
+
+  private
+
+  ## 'strong parameters' - whitelisting parameters allowed for mass assignment
+  # from UI web pages
+  def registration_params
+    params.require(:registration).permit(:company_no, :companyName)
+  end
+end
