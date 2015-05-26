d1cec9e6d44cfb50f3fbe6d58b9ef36c1cc13df6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2015-05-26 11:41:11 +0100

    Now postcode is being properly validated tests started to fail that were passing in an invalid postcode but expecting a validation message about it being unrecognised. Updated test value to truly be unrecognised rather than invalid [#92511498]

--- features/support/world_extensions.rb
+++ features/support/world_extensions.rb
@@ -8,7 +8,7 @@ module KnowsTheDomain
   end
 
   def my_unrecognised_postcode
-    @my_unrecognised_postcode ||= 'S015 OHX'
+    @my_unrecognised_postcode ||= 'BS1 4PW'
   end
 
   def agency_email_address
@@ -22,11 +22,11 @@ module KnowsTheDomain
   def my_finance_admin_user
     @my_finance_admin_user ||= FactoryGirl.create :finance_admin_user
   end
-  
+
   def my_finance_basic_user
     @my_finance_basic_user ||= FactoryGirl.create :finance_basic_user
   end
-  
+
   def my_agency_refund_user
     @my_agency_refund_user ||= FactoryGirl.create :agency_refund_user
   end
