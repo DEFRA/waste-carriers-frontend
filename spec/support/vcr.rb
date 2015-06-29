98295e08818e45bd4ad93e11e4abe10260775011
Tom Thurston <tomthurstonwork@live.com>
2015-06-29 15:30:58 +0100

    Refresh VCR cassettes every week, to guard against changes in external resources.

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -10,6 +10,6 @@ RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
   c.around(:each, :vcr) do |example|
     name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
-    VCR.use_cassette(name) { example.call }
+    VCR.use_cassette(name, re_record_interval: 7.days) { example.call }
   end
 end
