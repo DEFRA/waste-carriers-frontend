2c3dfd6a487b91156ec08cdc216c39762103b4cc
Tom Thurston <tomthurstonwork@live.com>
2016-05-13 12:00:06 +0100

    Fix error in method name, spotted during review.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -155,13 +155,13 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    notify_airbrake_async(exception)
+    notify_airbrake(exception)
     renderAccessDenied
   end
 
 
   rescue_from Errno::ECONNREFUSED do |exception|
-    notify_airbrake_async(exception)
+    notify_airbrake(exception)
     render :file => "/public/503.html", :status => 503
   end
 
