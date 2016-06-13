2a349a0cd82ca24c4e874c507609e68ac287e1e4
Lewis Buckley <lewis@lewisbuckley.co.uk>
2016-06-13 11:41:05 +0100

    Attach a PDF of the certificate to welcome email (#20)
    * Attach a PDF of the certificate to welcome email
    
    * Share certificate view between PDF and HTML
    
    Also update the wording of the welcome email to draw attention to the attachment
    
    * Remove old certificate view
    
    * Suggested tweak to end-page wording, and rename certificate PDF file to use registration number rather than database ID.
    
    * Tidy up PDF creation, use binary instead of gem

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -11,9 +11,13 @@ class RegistrationMailer < ActionMailer::Base
     @user = user
     @url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
     @registration = registration
+    @pdf = true
     email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    mail(to: @user.email, subject: I18n.t('global.mailer.complete.subject'),
-      from: email_with_name)
+    attachments["WasteCarrierRegistrationCertificate-#{registration.regIdentifier}.pdf"] = WickedPdf.new.pdf_from_string(
+      render_to_string(pdf: 'certificate', template: 'registrations/certificate', layout: 'pdf.html.erb'),
+      {}
+    )
+    mail(to: @user.email, subject: I18n.t('global.mailer.complete.subject'), from: email_with_name)
   end
 
   # Call via: RegistrationMailer.awaitingPayment_email(@user, @registration).deliver
@@ -22,9 +26,8 @@ class RegistrationMailer < ActionMailer::Base
     @url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
     @registration = registration
     email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    subjectMessage = I18n.t('global.mailer.payment.subject',:tier=>@registration.tier.downcase, :companyName=>@registration.companyName)
-    mail(to: @user.email, subject: subjectMessage,
-      from: email_with_name)
+    subjectMessage = I18n.t('global.mailer.payment.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
+    mail(to: @user.email, subject: subjectMessage, from: email_with_name)
   end
 
   # Call via: RegistrationMailer.awaitingConvictionsCheck_email(@user, @registration).deliver
@@ -33,17 +36,15 @@ class RegistrationMailer < ActionMailer::Base
     @url = ENV["WCRS_FRONTEND_PUBLIC_APP_DOMAIN"] || "www.wastecarriersregistration.service.gov.uk"
     @registration = registration
     email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    subjectMessage = I18n.t('global.mailer.convictions.subject',:tier=>@registration.tier.downcase, :companyName=>@registration.companyName)
-    mail(to: @user.email, subject: subjectMessage,
-      from: email_with_name)
+    subjectMessage = I18n.t('global.mailer.convictions.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
+    mail(to: @user.email, subject: subjectMessage, from: email_with_name)
   end
 
   # Call via: RegistrationMailer.account_already_confirmed_email(user, is_mid_registration).deliver
   def account_already_confirmed_email(user, is_mid_registration)
     @is_mid_registration = is_mid_registration
-    mail(
-      to: user.email,
-      subject: I18n.t('registration_mailer.account_already_confirmed.title'),
-      from: "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>")
+    subject = I18n.t('registration_mailer.account_already_confirmed.title')
+    from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    mail(to: user.email, subject: subject, from: from_address)
   end
 end
