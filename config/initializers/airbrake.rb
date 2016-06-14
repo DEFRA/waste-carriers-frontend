49d723420af4e85397cefa198e8250c131c430b8
lewispb <lewis@lewisbuckley.co.uk>
2016-06-14 11:31:39 +0100

    Resolve Airbrake project_id error

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,7 +1,10 @@
 if ENV['WCRS_FRONTEND_USE_AIRBRAKE'] && !Rails.env.test?
+
+  project_id = ENV['WCRS_FRONTEND_AIRBRAKE_PROJECT_ID'].to_i
+
   Airbrake.configure do |config|
     config.host = ENV['WCRS_FRONTEND_AIRBRAKE_HOST']
-    config.project_id = ENV['WCRS_FRONTEND_AIRBRAKE_PROJECT_ID']
+    config.project_id = project_id.zero? ? 1 : project_id
     config.project_key = ENV['WCRS_FRONTEND_AIRBRAKE_PROJECT_KEY']
     config.root_directory = Rails.root
     config.blacklist_keys = [/password/i]
