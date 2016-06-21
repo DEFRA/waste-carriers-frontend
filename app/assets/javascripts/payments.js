3f051ffb131f32d4f8e8bc844b30627810f7d307
Lewis Buckley <lewis@lewisbuckley.co.uk>
2016-06-21 11:13:06 +0100

    Account for an outstanding balance when paying

--- app/assets/javascripts/payments.js
+++ app/assets/javascripts/payments.js
@@ -14,6 +14,8 @@ function updateCostsWhenCopyCardQuantityChanged() {
         no_of_cards = 0;
     }
 
+    var total_fee = 0;
+    var outstanding_balance_obj = document.getElementById('registration_outstanding_balance');
     var card_fee_obj = document.getElementById('registration_copy_card_fee');
     var total_fee_obj = document.getElementById('registration_total_fee');
     var registration_fee_obj = document.getElementById('total_excluding_copy_cards');
@@ -22,11 +24,17 @@ function updateCostsWhenCopyCardQuantityChanged() {
 
     // Add extra handling to cope with orders that do not have registration fee's
     if (registration_fee_obj !== null) {
-        total_fee_obj.value =  Number(Number(card_fee_obj.value) + Number(registration_fee_obj.value)).toFixed(2)
+        total_fee = Number(card_fee_obj.value) + Number(registration_fee_obj.value)
     }
     else {
-        total_fee_obj.value =  Number(Number(card_fee_obj.value)).toFixed(2)
+        total_fee = Number(card_fee_obj.value)
     }
+
+    if (outstanding_balance_obj !== null) {
+      total_fee += Number(outstanding_balance_obj.value)
+    }
+
+    total_fee_obj.value = Number(total_fee).toFixed(2);
 }
 
 
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1031,16 +1031,20 @@ class Registration < Ohm::Model
     tier.inquiry.LOWER?
   end
 
-  def paid_in_full?
+  def outstanding_balance
     begin
       the_balance = self.try(:finance_details).try(:first).try(:balance)
       the_balance = 0 if the_balance.nil?
     rescue Exception => e
       Airbrake.notify(e)
       Rails.logger.debug e.message
+    else
+      return the_balance.to_i
     end
+  end
 
-    the_balance.to_i <= 0
+  def paid_in_full?
+    outstanding_balance <= 0
   end
 
   def self.business_type_options_for_select
