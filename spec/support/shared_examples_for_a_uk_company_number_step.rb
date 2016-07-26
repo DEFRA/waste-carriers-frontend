0db21820e82968ac180ae7ba7aba7c8b7aa49e8b
Tom Thurston <tomthurstonwork@live.com>
2016-07-26 13:11:23 +0100

    Convert shared_exemples_for_a_uk_company_number_step.rb to use expect-style assertions, rather than should-style.

--- spec/support/shared_examples_for_a_uk_company_number_step.rb
+++ spec/support/shared_examples_for_a_uk_company_number_step.rb
@@ -4,14 +4,16 @@ shared_examples_for 'a uk company number step' do
     expect(subject.foreign_limited_company?).to eq(false)
   end
 
-  it { should validate_presence_of(:company_no).with_message(/You must enter/) }
+  it 'should validate that the company number is not blank' do
+    expect(subject).to validate_presence_of(:company_no).with_message(/You must enter/)
+  end
 
   it 'should not allow company which is not active', :vcr do
-    subject.should_not allow_value('05868270').for(:company_no)
+    expect(subject).not_to allow_value('05868270').for(:company_no)
   end
 
   it 'should allow active company', :vcr do
-    subject.should allow_value('02050399').for(:company_no)
+    expect(subject).to allow_value('02050399').for(:company_no)
   end
 
   context 'check format only' do
@@ -19,11 +21,17 @@ shared_examples_for 'a uk company number step' do
       allow_any_instance_of(CompaniesHouseCaller).to receive(:status).and_return(:active)
     end
 
-    it { should allow_value('06731292', '6731292', '07589628', '7589628', '00000001',
-      '1', 'ni123456', 'NI123456', 'RO123456', 'SC123456', 'OC123456', 'SO123456',
-      'NC123456', 'AC097609').for(:company_no) }
+    it 'should allow valid company numbers' do
+      expect(subject).to allow_value('06731292', '6731292', '07589628', '7589628',
+          '00000001', '1', 'ni123456', 'NI123456', 'RO123456', 'SC123456', 'OC123456',
+          'SO123456', 'NC123456', 'AC097609'
+      ).for(:company_no)
+    end
 
-    it { should_not allow_value('NII12345', 'NI1234567', '123456789', '0', '00000000',
-      '-12345678', '-1234567').for(:company_no) }
+    it 'should not allow invalid company numbers' do
+      expect(subject).should_not allow_value('NII12345', 'NI1234567', '123456789', '0',
+          '00000000', '-12345678', '-1234567'
+      ).for(:company_no)
+    end
   end
 end
