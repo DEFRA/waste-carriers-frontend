62c626f26835899dfa7fbd9ec70ed69759a9cfcb
Lewis Buckley <lewis@lewisbuckley.co.uk>
2016-10-15 17:56:11 +0100

    Add registration URL params to more cucumber specs

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -9,11 +9,11 @@ class PasswordsController < Devise::PasswordsController
   def after_sending_reset_password_instructions_path_for(resource_name)
     redirect_to = super
     if resource_name == :user && session.key?(:at_mid_registration_signin_step)
-      redirect_to = signin_path
+      redirect_to = signin_path(reg_uuid: session[:at_mid_registration_signin_step])
     end
     redirect_to
   end
-  
+
   # After a user updates their password, should we redirect them to the normal
   # 'your registrations' page (with the user signed-in), or quietly sign them
   # out so they can continue with a registration in another window?
@@ -24,7 +24,7 @@ class PasswordsController < Devise::PasswordsController
       # of a new registration.  Session time-out will handle the cases where
       # the user doesn't return to their original tab / window.
       sign_out resource
-      redirect_to = mid_registration_password_changed_path
+      redirect_to = mid_registration_password_changed_path(reg_uuid: session[:at_mid_registration_signin_step])
     end
     redirect_to
   end
