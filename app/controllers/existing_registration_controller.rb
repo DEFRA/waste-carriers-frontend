81daa60d303e172cc6c25fb72e682e4f2ea75c88
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-02-01 17:14:25 +0000

    Refactor existing IR logic in existing_reg_ctrlr
    We will be adding new logic to handle new style registrations, which in turn will make the `ExistingRegistrationController` grow.
    
    So prior to that this change refactors the code so the logic for determining if the entered registration number is a valid IR registration is moved to a new method.
    
    This will help reduce complexity in the controller method and make its behaviour clearer.

--- app/controllers/existing_registration_controller.rb
+++ app/controllers/existing_registration_controller.rb
@@ -24,35 +24,37 @@ class ExistingRegistrationController < ApplicationController
       logger.debug "Current registration matched, Redirect to user sign in"
       redirect_to :new_user_session
       return
-    elsif(valid_ir_format? @registration.originalRegistrationNumber)
-      # Legacy regNo matched, check old format
-
-      # Call IR services to import IR registraion data
-      ir_registration = Registration.find_by_ir_number(@registration.originalRegistrationNumber)
-
-      if ir_registration.present?
-        # IR data found, merge with registration in redis
-        # Access Code and reg_uuid should not get overriden with IR data
-        @registration.add(ir_registration.attributes.except(:reg_uuid, :accessCode))
-        @registration.save
-
-        logger.debug "Legacy registration matched, Redirect to smart answers"
-        redirect_to(:business_type) and return
-      else
-        # No IR data found
-        @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.invalid_registration_number'))
-      end
-      # Error not matched
-    else
-      @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.invalid_registration_number'))
+    elsif existing_ir_registration?
+      logger.debug "Legacy registration matched, Redirect to smart answers"
+      redirect_to(:business_type) and return
     end
 
-    # Error must have occured, re-render view
+    # If we are here there must be validation errors so add a validation error
+    # and re-render view with a 400 status
+    @registration.errors.add(:originalRegistrationNumber, I18n.t('errors.messages.invalid_registration_number'))
+
     render 'show', status: :bad_request
 
   end
 
   private
+
+  def existing_ir_registration?
+    exists = false
+    return exists unless valid_ir_format?(@registration.originalRegistrationNumber)
+
+    # Call IR services to import IR registraion data
+    ir_registration = Registration.find_by_ir_number(@registration.originalRegistrationNumber)
+
+    if ir_registration.present?
+      # IR data found, merge with registration in redis
+      # Access Code and reg_uuid should not get overriden with IR data
+      @registration.add(ir_registration.attributes.except(:reg_uuid, :accessCode))
+      @registration.save
+      exists = true
+    end
+    exists
+  end
 
   def registration_params
     params.require(:registration).permit(:originalRegistrationNumber)
