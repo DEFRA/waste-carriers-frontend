96d1adcb8db30116e85497e52d2894bb90e59c5a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-02-07 14:22:10 +0000

    Remove redundant it works test
    Now we have other unit tests we don't need this anymore.

--- spec/models/irrenewal_spec.rb
+++ spec/models/irrenewal_spec.rb
@@ -39,12 +39,4 @@ describe Irrenewal do
       end
     end
   end
-
-  context "Renewing a registration" do
-    subject(:ir_renewal) { build(:irrenewal, :company) }
-
-    it "Just works" do
-      expect(ir_renewal.applicantType).to eq('Company')
-    end
-  end
 end
