3716059594569979741122de5c5e9603d0fa98b4
Iris Faraway <iris.faraway@gmail.com>
2018-02-14 11:19:09 +0000

    Add tests to clarify renewal window behaviour (#117)
    These tests are mostly for documentation as there was a lot of confusion around how the dates worked (normally we'd want to avoid directly testing private methods). Users can renew from 3 months before the expiry date – so if it expires on 25 May, they can renew from 25 February to 24 May. This PR also corrects the date which users are told they can renew from.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe ApplicationHelper do
   describe '#date_can_renew_from' do
     context 'when the date passed in is 2018-03-25 and the renewal window is 3 months' do
-      it 'returns a date of 2017-12-26' do
+      it 'returns a date of 2017-12-25' do
         Rails.configuration.stub(:registration_renewal_window).and_return(3.months)
 
         test_date = Date.parse("2018-03-25-T12:00:00.000Z")
-        expect(helper.date_can_renew_from(test_date)).to eq(Date.new(2017,12,26))
+        expect(helper.date_can_renew_from(test_date)).to eq(Date.new(2017,12,25))
       end
     end
   end
