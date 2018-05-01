29b62a65f1b8d826ee25d30c0a090ba959c92b93
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-05-01 14:06:22 +0100

    Synch secret key base & devise init. with renewals
    https://eaflood.atlassian.net/browse/WC-306
    
    In order to allow a user to move between the two systems (renewals and frontend) we need to ensure that devise is using the same `SECRET_KEY_BASE` across both.
    
    The frontend was specifically using one env var to initialise Devise, and another in the app (for setting secure cookies etc), none of which were using the `secrets.yml` method in later services.
    
    This change essentially copies the `secrets.yml` from renewals service, and removes the ad-hoc processes it was using and lets Rails use its default methods for handling the `SECRET_KEY_BASE`.

--- config/initializers/secret_token.rb
+++
@@ -1,9 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-
-Registrations::Application.config.secret_key_base = ENV['WCRS_FRONTEND_SECRET_KEY_BASE'] || SecureRandom.hex(64)
-
