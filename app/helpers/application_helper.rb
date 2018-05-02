b350ef012af998e0a8b99fd1cc7fccbbeac55874
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-05-02 09:58:03 +0100

    Arrgh! Remove interloping tabs from code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def session_inactivity_timeout_time
-  	if session[:last_seen_at]
+    if session[:last_seen_at]
       last_seen_at = session[:last_seen_at].to_datetime
       timeout = last_seen_at + Rails.application.config.app_session_inactivity_timeout
       timeout
-  	end
+    end
   end
 
   def createTitle(pageTitle)
