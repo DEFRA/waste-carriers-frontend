e4495236a4d868eb8c701c4c9c8b7cacf94ec47b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-06-25 23:53:21 +0100

    Standardise use XVFB for PDF env var
    This change updates the name of the env var that holds whether we should be using XVFB for generating PDF's. Essentially this is switched on in production, and when working in the vagrant box. Outside that and its up to the user to install and setup the ability to generate PDF's.

--- config/initializers/wickedpdf.rb
+++ config/initializers/wickedpdf.rb
@@ -1,4 +1,4 @@
-if ENV.has_key?('WCRS_FRONTEND_USE_XVFB_FOR_WICKEDPDF')
+if ENV.has_key?('WCRS_USE_XVFB_FOR_WICKEDPDF')
   WickedPdf.config = {
     exe_path: Rails.root.join('script', 'wkhtmltopdf.sh').to_s
   }
