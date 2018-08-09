7eb22d90dd4e62b1ca65be48ea0d011595e82a26
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-08-09 13:49:13 +0100

    Update email settings and add test email rake task
    https://eaflood.atlassian.net/browse/WC-393
    
    This change updates the email configuration in the app for 3 different reasons
    
    1. It brings the settings inline with the WEX and FRAE services so that the way we configure ActionMailer is consistent
    2. That we default to using mailcatcher in our development environment, but that we also allow the ability to override it in order to test sending email with Sendgrid
    3. We have added a rake task to allow us to quickly test if emails can be sent from the app once deployed

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,10 +1,9 @@
 class RegistrationMailer < ActionMailer::Base
   helper :application
   helper :registrations
-  default from: "registrations@wastecarriersregistration.service.gov.uk"
 
   # This line should have set the default from name, but didnt in testing,
-  # as such email_with_name variables were created to pass in the email name
+  # as such from_address variables were created to pass in the email name
   #default :from => "\"EA Waste Carriers\" <registrations@wastecarriersregistration.service.gov.uk>"
 
   def welcome_email(user, registration)
@@ -12,12 +11,13 @@ class RegistrationMailer < ActionMailer::Base
     @url = Rails.configuration.subdomain
     @registration = registration
     @pdf = true
-    email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    subject = I18n.t('global.mailer.complete.subject')
     attachments["WasteCarrierRegistrationCertificate-#{registration.regIdentifier}.pdf"] = WickedPdf.new.pdf_from_string(
       render_to_string(pdf: 'certificate', template: 'registrations/certificate', layout: 'pdf.html.erb'),
       {}
     )
-    mail(to: @user.email, subject: I18n.t('global.mailer.complete.subject'), from: email_with_name)
+    mail(to: @user.email, subject: subject, from: from_address)
   end
 
   # Call via: RegistrationMailer.awaitingPayment_email(@user, @registration).deliver_now
@@ -25,9 +25,9 @@ class RegistrationMailer < ActionMailer::Base
     @user = user
     @url = Rails.configuration.subdomain
     @registration = registration
-    email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    subjectMessage = I18n.t('global.mailer.payment.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
-    mail(to: @user.email, subject: subjectMessage, from: email_with_name)
+    from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    subject = I18n.t('global.mailer.payment.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
+    mail(to: @user.email, subject: subject, from: from_address)
   end
 
   # Call via: RegistrationMailer.awaitingConvictionsCheck_email(@user, @registration).deliver_now
@@ -35,16 +35,24 @@ class RegistrationMailer < ActionMailer::Base
     @user = user
     @url = Rails.configuration.subdomain
     @registration = registration
-    email_with_name = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
-    subjectMessage = I18n.t('global.mailer.convictions.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
-    mail(to: @user.email, subject: subjectMessage, from: email_with_name)
+    from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    subject = I18n.t('global.mailer.convictions.subject', tier: @registration.tier.downcase, companyName: @registration.companyName)
+    mail(to: @user.email, subject: subject, from: from_address)
   end
 
   # Call via: RegistrationMailer.account_already_confirmed_email(user, is_mid_registration).deliver_now
   def account_already_confirmed_email(user, is_mid_registration)
     @is_mid_registration = is_mid_registration
-    subject = I18n.t('registration_mailer.account_already_confirmed.title')
     from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    subject = I18n.t('registration_mailer.account_already_confirmed.title')
     mail(to: user.email, subject: subject, from: from_address)
+  end
+
+  def test_email
+    from_address = "#{Rails.configuration.registrations_service_emailName} <#{Rails.configuration.registrations_service_email}>"
+    subject = "Waste Carriers Test email"
+    mail(to: Rails.configuration.email_test_address, subject: subject, from: from_address) do |format|
+      format.text(content_type: "text/plain", charset: "UTF-8", content_transfer_encoding: "7bit")
+    end
   end
 end
