43fd3300c98a4ff2efb3d889a7926a535f2c2d37
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2018-10-26 22:38:05 +0100

    Add grace window config plkus rspec helpers
    Add a new configuration value which can be driven by an env var (but has a default of 3 days which matches the current instruction from NCCC) for the grace window.
    
    Also start to use this value in our tests, along with some helpers to determine a date that is both inside and outside of it.
    
    (cherry picked from commit 2d22f4c9a40e4ef209cb275021f85b67f2fae849)

--- spec/models/concerns/can_be_renewed.rb
+++ spec/models/concerns/can_be_renewed.rb
@@ -151,9 +151,12 @@ shared_examples_for "can_be_renewed" do
   end
 
   describe "#in_expiry_grace_window?" do
+    let (:expires_on) { "1 Oct 2018".to_date }
+
     subject do
       registration = described_class.ctor
       registration.tier = "UPPER"
+      registration.expires_on = date_to_utc_milliseconds(expires_on)
       registration.metaData.first.update(status: "EXPIRED")
       registration
     end
@@ -165,17 +168,19 @@ shared_examples_for "can_be_renewed" do
       end
     end
 
-    context "when the expires_on date is within the window" do
+    context "when the current date is within the window" do
       it "returns true" do
-        subject.expires_on = date_to_utc_milliseconds(Date.today)
-        expect(subject.in_expiry_grace_window?).to eq(true)
+        Timecop.freeze(date_inside_grace_window(expires_on)) do
+          expect(subject.in_expiry_grace_window?).to eq(true)
+        end
       end
     end
 
-    context "when the expires_on date is outside the window" do
+    context "when the current date is outside the window" do
       it "returns false" do
-        subject.expires_on = date_to_utc_milliseconds(Date.today)
-        expect(subject.in_expiry_grace_window?).to eq(false)
+        Timecop.freeze(date_outside_grace_window(expires_on)) do
+          expect(subject.in_expiry_grace_window?).to eq(false)
+        end
       end
     end
   end
