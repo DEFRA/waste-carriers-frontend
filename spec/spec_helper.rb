2deec1032e744a89ef5f4ce08d6d7b72a64f504a
cintamani <cintamani.puddu@gmail.com>
2019-04-29 06:17:20 +0100

    Remove byebug and swap it with pry-byebug
    byebug has a very similar interface as gdb, but byebug does not use the powerful Pry REPL.
    binding.pry uses Pry, but lacks some of the byebug features.
    [pry-byebug](https://github.com/deivid-rodriguez/pry-byebug) brings some capabilities byebug to binding.pry, so using that, will give you the most debugging powers.
    
    Credits: https://docs.gitlab.com/ee/development/pry_debugging.html#byebug-vs-bindingpry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@ require 'capybara/email/rspec'
 require 'webmock/rspec'
 require_relative '../lib/test_helpers/database_cleaning'
 
+# Call "buinding.pry" anywhere in the code to stop execution and get a debugger console
+require "pry-byebug"
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 #
